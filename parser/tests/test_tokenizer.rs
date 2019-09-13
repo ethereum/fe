a03518c35bff999d21f50b39c4228a72125e8b87
David Sanders <davesque@gmail.com>
2019-09-13 12:55:29 -0600

    Cleanup and comments

--- parser/tests/test_tokenizer.rs
+++ parser/tests/test_tokenizer.rs
@@ -4,19 +4,11 @@ extern crate rust_vyper_parser;
 
 use std::fmt;
 
-use difference::{Changeset, Difference};
 use pyo3::prelude::*;
 use pyo3::types::PyBytes;
 use rust_vyper_parser::tokenizer::*;
 
-pub struct Diff(Changeset);
-
-impl Diff {
-    pub fn new(left: &str, right: &str) -> Self {
-        Self(Changeset::new(left, right, "\n"))
-    }
-}
-
+/// Return the lines in `lines` prefixed with the prefix in `prefix.
 fn prefix_lines(prefix: &str, lines: &str) -> String {
     lines
         .lines()
@@ -25,17 +17,26 @@ fn prefix_lines(prefix: &str, lines: &str) -> String {
         .join("\n")
 }
 
+/// Wrapper struct for formatting changesets from the `difference` package.
+pub struct Diff(difference::Changeset);
+
+impl Diff {
+    pub fn new(left: &str, right: &str) -> Self {
+        Self(difference::Changeset::new(left, right, "\n"))
+    }
+}
+
 impl fmt::Display for Diff {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         for d in &self.0.diffs {
             match *d {
-                Difference::Same(ref x) => {
+                difference::Difference::Same(ref x) => {
                     write!(f, "{}{}", prefix_lines(" ", x), self.0.split)?;
                 }
-                Difference::Add(ref x) => {
+                difference::Difference::Add(ref x) => {
                     write!(f, "\x1b[92m{}\x1b[0m{}", prefix_lines("+", x), self.0.split)?;
                 }
-                Difference::Rem(ref x) => {
+                difference::Difference::Rem(ref x) => {
                     write!(f, "\x1b[91m{}\x1b[0m{}", prefix_lines("-", x), self.0.split)?;
                 }
             }
@@ -44,6 +45,7 @@ impl fmt::Display for Diff {
     }
 }
 
+/// Compare the given strings and panic when not equal with a colorized line diff.
 macro_rules! assert_strings_eq {
     ($left:expr , $right:expr,) => ({
         assert_eq!($left, $right)
@@ -75,6 +77,8 @@ macro_rules! assert_strings_eq {
     });
 }
 
+/// Load the `token_helpers.py` module for generating tokenizations using the python std lib
+/// `tokenize` module.
 fn get_token_helpers<'p>(py: Python<'p>) -> PyResult<&'p PyModule> {
     PyModule::from_code(
         py,
@@ -86,10 +90,12 @@ fn get_token_helpers<'p>(py: Python<'p>) -> PyResult<&'p PyModule> {
 
 #[test]
 fn test_tokenize() {
+    // Generate tokenization using rust
     let test_py = include_str!("fixtures/tokenizer/test.py");
     let test_py_tokens = tokenize(test_py).unwrap();
     let actual_token_json = serde_json::to_string_pretty(&test_py_tokens).unwrap();
 
+    // Generate known-good tokenization using python
     let gil = Python::acquire_gil();
     let py = gil.python();
     let token_helpers = get_token_helpers(py).unwrap();
