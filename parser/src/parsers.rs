4d6b520176dd143d21c90d42dfd695c15b16590c
David Sanders <davesque@gmail.com>
2019-09-15 16:39:10 -0600

    Rewrite parsers to use token slices

--- parser/src/parsers.rs
+++ parser/src/parsers.rs
@@ -1,13 +1,31 @@
 use nom::bytes::complete::{tag, take_while1};
 use nom::character::complete::{char, multispace1, space0, space1};
 use nom::combinator::{opt, verify};
-use nom::error::{context, ParseError};
-use nom::multi::many0;
+use nom::error::{context, ErrorKind, ParseError};
+use nom::multi::{many0, many1};
 use nom::sequence::{preceded, terminated};
 use nom::{Err as NomErr, IResult, InputTake};
 
 use crate::ast::ModuleStmt::*;
 use crate::ast::*;
+use crate::errors::make_error;
+
+use crate::tokenizer::tokenize::tokenize;
+use crate::tokenizer::types::{TokenInfo, TokenType};
+
+type TokenRef<'a> = &'a TokenInfo<'a>;
+type TokenSlice<'a> = &'a [TokenInfo<'a>];
+type TokenResult<'a, O, E> = IResult<TokenSlice<'a>, O, E>;
+
+/// Tokenize the given source code in `source` and filter out tokens not relevant to parsing.
+pub fn get_parse_tokens<'a>(source: &'a str) -> Result<Vec<TokenInfo<'a>>, String> {
+    let tokens = tokenize(source)?;
+
+    Ok(tokens
+        .into_iter()
+        .filter(|t| t.typ != TokenType::NL && t.typ != TokenType::COMMENT)
+        .collect())
+}
 
 /// Return true if the char `c` is a valid identifier character.
 pub fn is_identifier_char(c: char) -> bool {
@@ -156,6 +174,180 @@ where
     }
 }
 
+/// Parse a vyper source file into a `Module` AST object.
+pub fn parse_token_file<'a, E>(input: TokenSlice<'a>) -> TokenResult<'a, Module, E>
+where
+    E: ParseError<TokenSlice<'a>>,
+{
+    // Consume any leading newlines
+    let (i, _) = many0(newline_token)(input)?;
+
+    // module_stmt*
+    let (i, body) = many0(parse_token_module_stmt)(i)?;
+
+    // <endmarker>
+    let (i, _) = endmarker_token(i)?;
+
+    Ok((i, Module { body }))
+}
+
+/// Parse a module statement, such as an event or contract definition, into a `ModuleStmt` object.
+pub fn parse_token_module_stmt<'a, E>(input: TokenSlice<'a>) -> TokenResult<'a, ModuleStmt, E>
+where
+    E: ParseError<TokenSlice<'a>>,
+{
+    let (i, module_stmt) = context("expected event definition", parse_token_event_def)(input)?;
+
+    Ok((i, module_stmt))
+}
+
+/// Parse an event definition statement into a `ModuleStmt::EventDef` object.
+pub fn parse_token_event_def<'a, E>(input: TokenSlice<'a>) -> TokenResult<'a, ModuleStmt, E>
+where
+    E: ParseError<TokenSlice<'a>>,
+{
+    // "event" name ":" <newline>
+    let (i, _) = name_string("event")(input)?;
+    let (i, name) = name_token(i)?;
+    let (i, _) = op_string(":")(i)?;
+    let (i, _) = newline_token(i)?;
+
+    // <indent> event_field* <dedent>
+    let (i, _) = indent_token(i)?;
+    let (i, fields) = many1(parse_token_event_field)(i)?;
+    let (i, _) = dedent_token(i)?;
+
+    Ok((
+        i,
+        EventDef {
+            name: name.string.to_string(),
+            fields: fields,
+        },
+    ))
+}
+
+/// Parse an event field definition into an `EventField` object.
+pub fn parse_token_event_field<'a, E>(input: TokenSlice<'a>) -> TokenResult<'a, EventField, E>
+where
+    E: ParseError<TokenSlice<'a>>,
+{
+    let (i, name) = name_token(input)?;
+    let (i, _) = op_string(":")(i)?;
+    let (i, typ) = name_token(i)?;
+    let (i, _) = newline_token(i)?;
+
+    Ok((
+        i,
+        EventField {
+            name: name.string.to_string(),
+            typ: typ.string.to_string(),
+        },
+    ))
+}
+
+/// Parse a single token from a token slice.
+pub fn one_token<'a, E>(input: TokenSlice<'a>) -> TokenResult<'a, TokenRef<'a>, E>
+where
+    E: ParseError<TokenSlice<'a>>,
+{
+    match input.iter().next() {
+        None => make_error(input, ErrorKind::Eof),
+        Some(token) => Ok((&input[1..], token)),
+    }
+}
+
+/// Parse a token of a specific type from a token slice.
+pub fn token<'a, E>(typ: TokenType) -> impl Fn(TokenSlice<'a>) -> TokenResult<'a, TokenRef<'a>, E>
+where
+    E: ParseError<TokenSlice<'a>>,
+{
+    verify(one_token, move |t: &TokenInfo| t.typ == typ)
+}
+
+/// Parse a name token from a token slice.
+pub fn name_token<'a, E>(input: TokenSlice<'a>) -> TokenResult<'a, TokenRef<'a>, E>
+where
+    E: ParseError<TokenSlice<'a>>,
+{
+    token(TokenType::NAME)(input)
+}
+
+/// Parse a name token containing a specific string from a token slice.
+pub fn name_string<'a, E>(
+    string: &'a str,
+) -> impl Fn(TokenSlice<'a>) -> TokenResult<'a, TokenRef<'a>, E>
+where
+    E: ParseError<TokenSlice<'a>>,
+{
+    verify(name_token, move |t: &TokenInfo| t.string == string)
+}
+
+/// Parse an op token from a token slice.
+pub fn op_token<'a, E>(input: TokenSlice<'a>) -> TokenResult<'a, TokenRef<'a>, E>
+where
+    E: ParseError<TokenSlice<'a>>,
+{
+    token(TokenType::OP)(input)
+}
+
+/// Parse an op token containing a specific string from a token slice.
+pub fn op_string<'a, E>(
+    string: &'a str,
+) -> impl Fn(TokenSlice<'a>) -> TokenResult<'a, TokenRef<'a>, E>
+where
+    E: ParseError<TokenSlice<'a>>,
+{
+    verify(op_token, move |t: &TokenInfo| t.string == string)
+}
+
+/// Parse a number token from a token slice.
+pub fn number_token<'a, E>(input: TokenSlice<'a>) -> TokenResult<'a, TokenRef<'a>, E>
+where
+    E: ParseError<TokenSlice<'a>>,
+{
+    token(TokenType::NUMBER)(input)
+}
+
+/// Parse a string token from a token slice.
+pub fn string_token<'a, E>(input: TokenSlice<'a>) -> TokenResult<'a, TokenRef<'a>, E>
+where
+    E: ParseError<TokenSlice<'a>>,
+{
+    token(TokenType::STRING)(input)
+}
+
+/// Parse an indent token from a token slice.
+pub fn indent_token<'a, E>(input: TokenSlice<'a>) -> TokenResult<'a, TokenRef<'a>, E>
+where
+    E: ParseError<TokenSlice<'a>>,
+{
+    token(TokenType::INDENT)(input)
+}
+
+/// Parse a dedent token from a token slice.
+pub fn dedent_token<'a, E>(input: TokenSlice<'a>) -> TokenResult<'a, TokenRef<'a>, E>
+where
+    E: ParseError<TokenSlice<'a>>,
+{
+    token(TokenType::DEDENT)(input)
+}
+
+/// Parse a grammatically significant newline token from a token slice.
+pub fn newline_token<'a, E>(input: TokenSlice<'a>) -> TokenResult<'a, TokenRef<'a>, E>
+where
+    E: ParseError<TokenSlice<'a>>,
+{
+    token(TokenType::NEWLINE)(input)
+}
+
+/// Parse an endmarker token from a token slice.
+pub fn endmarker_token<'a, E>(input: TokenSlice<'a>) -> TokenResult<'a, TokenRef<'a>, E>
+where
+    E: ParseError<TokenSlice<'a>>,
+{
+    token(TokenType::ENDMARKER)(input)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -163,6 +355,7 @@ mod tests {
     use nom::error::{ErrorKind, ErrorKind::*};
 
     use crate::errors::make_error;
+    use crate::tokenizer::tokenize;
 
     type SimpleError<I> = (I, ErrorKind);
 
@@ -225,10 +418,11 @@ mod tests {
     fn test_parse_file() {
         // Empty file
         let examples = vec!["", "  \t ", " \n\n   \t \n \t "];
-        let expected: IResult<_, _, SimpleError<_>> = Ok(("", Module { body: vec![] }));
+        let expected: IResult<_, _, SimpleError<_>> = Ok((&[][..], Module { body: vec![] }));
 
         for inp in examples {
-            let actual = parse_file::<SimpleError<_>>(inp);
+            let tokens = get_parse_tokens(inp).unwrap();
+            let actual = parse_token_file::<SimpleError<_>>(&tokens[..]);
             assert_eq!(actual, expected);
         }
 
@@ -251,7 +445,7 @@ event Greet:
 ",
         ];
         let expected: IResult<_, _, SimpleError<_>> = Ok((
-            "",
+            &[][..],
             Module {
                 body: vec![EventDef {
                     name: "Greet".to_string(),
@@ -269,7 +463,8 @@ event Greet:
             },
         ));
         for inp in examples {
-            let actual = parse_file::<SimpleError<_>>(inp);
+            let tokens = get_parse_tokens(inp).unwrap();
+            let actual = parse_token_file::<SimpleError<_>>(&tokens[..]);
             assert_eq!(actual, expected);
         }
 
@@ -311,7 +506,7 @@ event Other:
 ",
         ];
         let expected: IResult<_, _, SimpleError<_>> = Ok((
-            "",
+            &[][..],
             Module {
                 body: vec![
                     EventDef {
@@ -344,7 +539,8 @@ event Other:
             },
         ));
         for inp in examples {
-            let actual = parse_file::<SimpleError<_>>(inp);
+            let tokens = get_parse_tokens(inp).unwrap();
+            let actual = parse_token_file::<SimpleError<_>>(&tokens[..]);
             assert_eq!(actual, expected);
         }
     }
