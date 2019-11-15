5179149444623037de1a3ce28886d21457021bab
David Sanders <davesque@gmail.com>
2019-11-15 15:34:53 -0700

    Move diff formatting into testing utils module

--- parser/tests/test_tokenizer.rs
+++ parser/tests/test_tokenizer.rs
@@ -1,5 +1,3 @@
-use std::fmt;
-
 use pyo3::prelude::*;
 use pyo3::types::PyBytes;
 use serde::{
@@ -9,76 +7,9 @@ use serde::{
 
 use vyper_parser::tokenizer::*;
 
-/// Return the lines of text in the string `lines` prefixed with the prefix in
-/// the string `prefix`.
-fn prefix_lines(prefix: &str, lines: &str) -> String {
-    lines
-        .lines()
-        .map(|i| [prefix, i].concat())
-        .collect::<Vec<String>>()
-        .join("\n")
-}
-
-/// Wrapper struct for formatting changesets from the `difference` package.
-pub struct Diff(difference::Changeset);
-
-impl Diff {
-    pub fn new(left: &str, right: &str) -> Self {
-        Self(difference::Changeset::new(left, right, "\n"))
-    }
-}
-
-impl fmt::Display for Diff {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        for d in &self.0.diffs {
-            match *d {
-                difference::Difference::Same(ref x) => {
-                    write!(f, "{}{}", prefix_lines(" ", x), self.0.split)?;
-                }
-                difference::Difference::Add(ref x) => {
-                    write!(f, "\x1b[92m{}\x1b[0m{}", prefix_lines("+", x), self.0.split)?;
-                }
-                difference::Difference::Rem(ref x) => {
-                    write!(f, "\x1b[91m{}\x1b[0m{}", prefix_lines("-", x), self.0.split)?;
-                }
-            }
-        }
-        Ok(())
-    }
-}
-
-/// Compare the given strings and panic when not equal with a colorized line
-/// diff.
-macro_rules! assert_strings_eq {
-    ($left:expr , $right:expr,) => ({
-        assert_strings_eq!($left, $right)
-    });
-    ($left:expr , $right:expr) => ({
-        match (&($left), &($right)) {
-            (left_val, right_val) => {
-                if *left_val != *right_val {
-                    panic!(
-                        "assertion failed: `(left == right)`\ndiff:\n{}",
-                        Diff::new(left_val, right_val),
-                    )
-                }
-            }
-        }
-    });
-    ($left:expr , $right:expr, $($arg:tt)*) => ({
-        match (&($left), &($right)) {
-            (left_val, right_val) => {
-                if *left_val != *right_val {
-                    panic!(
-                        "assertion failed: `(left == right)`: {}\ndiff:\n{}",
-                        format_args!($($arg)*),
-                        Diff::new(left_val, right_val),
-                    )
-                }
-            }
-        }
-    });
-}
+#[macro_use]
+mod utils;
+use utils::Diff;
 
 pub struct TokenHelpers<'a> {
     py: Python<'a>,
---
+++ parser/tests/utils/mod.rs
@@ -0,0 +1,72 @@
+use std::fmt;
+
+/// Return the lines of text in the string `lines` prefixed with the prefix in
+/// the string `prefix`.
+fn prefix_lines(prefix: &str, lines: &str) -> String {
+    lines
+        .lines()
+        .map(|i| [prefix, i].concat())
+        .collect::<Vec<String>>()
+        .join("\n")
+}
+
+/// Wrapper struct for formatting changesets from the `difference` package.
+pub struct Diff(difference::Changeset);
+
+impl Diff {
+    pub fn new(left: &str, right: &str) -> Self {
+        Self(difference::Changeset::new(left, right, "\n"))
+    }
+}
+
+impl fmt::Display for Diff {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for d in &self.0.diffs {
+            match *d {
+                difference::Difference::Same(ref x) => {
+                    write!(f, "{}{}", prefix_lines(" ", x), self.0.split)?;
+                }
+                difference::Difference::Add(ref x) => {
+                    write!(f, "\x1b[92m{}\x1b[0m{}", prefix_lines("+", x), self.0.split)?;
+                }
+                difference::Difference::Rem(ref x) => {
+                    write!(f, "\x1b[91m{}\x1b[0m{}", prefix_lines("-", x), self.0.split)?;
+                }
+            }
+        }
+        Ok(())
+    }
+}
+
+/// Compare the given strings and panic when not equal with a colorized line
+/// diff.
+macro_rules! assert_strings_eq {
+    ($left:expr , $right:expr,) => ({
+        assert_strings_eq!($left, $right)
+    });
+    ($left:expr , $right:expr) => ({
+        match (&($left), &($right)) {
+            (left_val, right_val) => {
+                if *left_val != *right_val {
+                    panic!(
+                        "assertion failed: `(left == right)`\ndiff:\n{}",
+                        Diff::new(left_val, right_val),
+                    )
+                }
+            }
+        }
+    });
+    ($left:expr , $right:expr, $($arg:tt)*) => ({
+        match (&($left), &($right)) {
+            (left_val, right_val) => {
+                if *left_val != *right_val {
+                    panic!(
+                        "assertion failed: `(left == right)`: {}\ndiff:\n{}",
+                        format_args!($($arg)*),
+                        Diff::new(left_val, right_val),
+                    )
+                }
+            }
+        }
+    });
+}
