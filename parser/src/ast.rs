6dae88261700d916edae3dd0d04f4f4078575960
David Sanders <davesque@gmail.com>
2019-11-19 12:10:42 -0700

    Add SourceSpan struct and trait impls

--- parser/src/ast.rs
+++ parser/src/ast.rs
@@ -5,8 +5,46 @@ use serde::{
     Serialize,
 };
 
+use crate::tokenizer::types::{
+    Offset,
+    Position,
+    TokenInfo,
+};
+
 pub type Name = String;
 pub type Type = String;
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct SourceSpan {
+    start_pos: Position,
+    start_off: Offset,
+    end_pos: Position,
+    end_off: Offset,
+}
+
+impl From<&TokenInfo<'_>> for SourceSpan {
+    fn from(token_info: &TokenInfo) -> Self {
+        Self {
+            start_pos: token_info.start_pos,
+            start_off: token_info.start_off,
+            end_pos: token_info.end_pos,
+            end_off: token_info.end_off,
+        }
+    }
+}
+
+impl From<(&TokenInfo<'_>, &TokenInfo<'_>)> for SourceSpan {
+    fn from(tokens: (&TokenInfo, &TokenInfo)) -> Self {
+        let (start, end) = tokens;
+
+        Self {
+            start_pos: start.start_pos,
+            start_off: start.start_off,
+            end_pos: end.end_pos,
+            end_off: end.end_off,
+        }
+    }
+}
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct Module {
