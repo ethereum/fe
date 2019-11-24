cfa8fe62440a37c9a30efae9d7ace93f83fa79b5
David Sanders <davesque@gmail.com>
2019-11-23 21:36:54 -0700

    Update token tests to use caching file positioner

--- parser/tests/test_tokenizer.rs
+++ parser/tests/test_tokenizer.rs
@@ -2,6 +2,10 @@ use pyo3::prelude::*;
 use pyo3::types::PyBytes;
 use serde::Serialize;
 
+use vyper_parser::string_utils::{
+    FilePositions,
+    Position,
+};
 use vyper_parser::tokenizer::*;
 
 #[macro_use]
@@ -50,11 +54,6 @@ impl<'a> TokenHelpers<'a> {
     }
 }
 
-type Position = (
-    usize, // a 1-indexed line number
-    usize, // a 0-indexed byte offset into a line
-);
-
 /// This struct and its associated `From` implementation are used to cast vyper
 /// tokens (which may include specialized information only used by the vyper
 /// parser, such as global byte offsets into a source file) into objects with
@@ -68,48 +67,37 @@ struct PythonTokenInfo<'a> {
     pub line: &'a str,
 }
 
-#[inline]
-fn offset_to_position(input: &str, offset: usize) -> Position {
-    let mut line: usize = 1;
-    let mut col: usize = 0;
+impl<'a> From<(&'a Token<'a>, &mut FilePositions<'_>)> for PythonTokenInfo<'a> {
+    fn from(info: (&'a Token<'a>, &mut FilePositions<'_>)) -> Self {
+        let (tok, file_positions) = info;
 
-    for (chr_off, chr) in input.char_indices() {
-        if chr_off >= offset {
-            break;
-        }
-
-        if chr == '\n' {
-            line += 1;
-            col = 0;
-        } else {
-            col += chr.len_utf8();
-        }
-    }
-
-    (line, col)
-}
-
-impl<'a> From<(&'a Token<'a>, &str)> for PythonTokenInfo<'a> {
-    fn from(info: (&'a Token<'a>, &str)) -> Self {
-        let (token, input) = info;
+        let start_pos = match file_positions.get_pos(tok.span.start) {
+            Some(pos) => pos,
+            None => file_positions.get_eof(),
+        };
+        let end_pos = match file_positions.get_pos(tok.span.end) {
+            Some(pos) => pos,
+            None => file_positions.get_eof(),
+        };
 
         Self {
-            typ: token.typ,
-            string: token.string,
-            start: offset_to_position(input, token.span.start),
-            end: offset_to_position(input, token.span.end),
-            line: token.line,
+            typ: tok.typ,
+            string: tok.string,
+            start: start_pos,
+            end: end_pos,
+            line: tok.line,
         }
     }
 }
 
 fn get_rust_token_json(input: &str) -> String {
     let tokens = tokenize(input).unwrap();
+    let mut file_positions = FilePositions::new(input);
 
     // Convert vyper tokens into python tokens
     let python_tokens = tokens
         .iter()
-        .map(|i| (i, input).into())
+        .map(|tok| (tok, &mut file_positions).into())
         .collect::<Vec<PythonTokenInfo>>();
 
     serde_json::to_string_pretty(&python_tokens).unwrap()
