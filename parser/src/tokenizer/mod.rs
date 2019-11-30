8a37038b07835b0da9ea3102ef4cdf8020ccf241
David Sanders <davesque@gmail.com>
2019-11-29 21:24:50 -0700

    Update tokenize mod docs

--- parser/src/tokenizer/mod.rs
+++ parser/src/tokenizer/mod.rs
@@ -13,28 +13,25 @@
 //! let token_vector = tokenize(source_string).unwrap();
 //! ```
 //!
-//! ## Similarities with tokenize.py
+//! ## Differences/similarities with python's `tokenize` module
 //!
-//! * The `self::tokenize::tokenize` function generates all token types produced
-//!   by python's `tokenize.tokenize` method *except for* an initial `ENCODING`
-//!   token.  Source files are always assumed to be encoded in utf-8 so this
-//!   token is redundant.
-//!
-//! ## Differences with tokenize.py
-//!
-//! * The `self::types::Token` struct, which we use to represented parsed
-//!   tokens, differs from python's `tokenize.TokenInfo` tuples in that it
-//!   includes a single `span` field containing the byte offsets representing
-//!   the beginning and exclusive end of the token in a source string.
-//!   `TokenInfo` instances use line/column tuples to represent the beginning
-//!   and ending positions of a token.
-//! * The `self::tokenize::tokenize` function has no streaming behavior and
+//! * The [`self::tokenize::tokenize`] function generates all token types
+//!   produced by python's `tokenize.tokenize` method *except* for an initial
+//!   `ENCODING` token.  Source files are always assumed to be encoded in utf-8
+//!   so this token is redundant.
+//! * The [`self::types::Token`] struct, which we use to represent parsed
+//!   tokens, includes a single `span` field containing the byte offsets
+//!   representing the beginning and exclusive end of the token in a source
+//!   string. This differs from Python's `TokenInfo` instances which use
+//!   line/column tuples to represent the beginning and ending positions of a
+//!   token.
+//! * The [`self::tokenize::tokenize`] function has no streaming behavior and
 //!   accepts a reference to an entire source string available in memory.
 //!   Python's `tokenize.tokenize` function accepts a reference to a function
-//!   that progressively yields lines of text from the source file.
+//!   that progressively yields lines of text from a source file.
 //!
-//! As python's `tokenize` module's implementation is pretty ugly, so is the
-//! implementation of `self::tokenize::tokenize`.  It may be a candidate for
+//! As Python's `tokenize` module's implementation is pretty ugly, so is the
+//! implementation of [`self::tokenize::tokenize`].  It may be a candidate for
 //! later cleanup.  However, it may also be fine as is assuming it doesn't need
 //! to be modified often.
 
