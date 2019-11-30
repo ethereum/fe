dea02d62fb47e95ae73066902f08fa7cc6d3e4d3
David Sanders <davesque@gmail.com>
2019-11-29 20:52:12 -0700

    Add doc comments for tokenizer module

--- parser/src/tokenizer/mod.rs
+++ parser/src/tokenizer/mod.rs
@@ -1,3 +1,43 @@
+//! A Rust port of [Python's std lib `tokenize`
+//! module](https://github.com/python/cpython/blob/2a58b0636d1f620f8a85a2e4c030cc10551936a5/Lib/tokenize.py).
+//!
+//! ## Usage
+//!
+//! ```rust
+//! use vyper_parser::tokenizer::tokenize;
+//!
+//! let source_string = r#"
+//! class Foo:
+//!     bar = "baz"
+//! "#;
+//! let token_vector = tokenize(source_string).unwrap();
+//! ```
+//!
+//! ## Similarities with tokenize.py
+//!
+//! * The `self::tokenize::tokenize` function generates all token types produced
+//!   by python's `tokenize.tokenize` method *except for* an initial `ENCODING`
+//!   token.  Source files are always assumed to be encoded in utf-8 so this
+//!   token is redundant.
+//!
+//! ## Differences with tokenize.py
+//!
+//! * The `self::types::Token` struct, which we use to represented parsed
+//!   tokens, differs from python's `tokenize.TokenInfo` tuples in that it
+//!   includes a single `span` field containing the byte offsets representing
+//!   the beginning and exclusive end of the token in a source string.
+//!   `TokenInfo` instances use line/column tuples to represent the beginning
+//!   and ending positions of a token.
+//! * The `self::tokenize::tokenize` function has no streaming behavior and
+//!   accepts a reference to an entire source string available in memory.
+//!   Python's `tokenize.tokenize` function accepts a reference to a function
+//!   that progressively yields lines of text from the source file.
+//!
+//! As python's `tokenize` module's implementation is pretty ugly, so is the
+//! implementation of `self::tokenize::tokenize`.  It may be a candidate for
+//! later cleanup.  However, it may also be fine as is assuming it doesn't need
+//! to be modified often.
+
 pub mod regex;
 pub mod tokenize;
 pub mod types;
