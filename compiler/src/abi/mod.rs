4a061d20947d4bf47a6debb489800a3c9407294d
Grant Wuerker <gwuerker@gmail.com>
2020-02-03 11:08:11 +0800

    Function selector.

--- compiler/src/abi/mod.rs
+++ compiler/src/abi/mod.rs
@@ -1,5 +1,7 @@
 use serde::{Serialize};
 use vyper_parser::ast as vyp;
+use tiny_keccak::{Keccak, Hasher};
+use crate::abi::VariableType::Uint256;
 
 #[derive(Serialize)]
 pub struct Contract {
@@ -42,6 +44,14 @@ enum FunctionType {
 #[serde(rename_all = "lowercase")]
 enum VariableType {
     Uint256
+}
+
+impl ToString for VariableType {
+    fn to_string(&self) -> String {
+        match self {
+            VariableType::Uint256 => String::from("uint256")
+        }
+    }
 }
 
 #[derive(Serialize)]
@@ -87,30 +97,62 @@ fn func_def_arg<'a>(arg: &'a vyp::FuncDefArg<'a>) -> Input {
     }
 }
 
-#[test]
-fn test_function_serialize() {
-    let func = Function {
-        name: String::from("foobar"),
-        typ: FunctionType::Function,
-        inputs: vec![
-            Input {
-                name: String::from("foo"),
-                typ: VariableType::Uint256
-            }
-        ],
-        outputs: vec![
-            Output {
-                name: String::from("bar"),
-                typ: VariableType::Uint256
-            }
-        ]
-    };
+fn func_select<'a>(stmt: &'a vyp::ContractStmt<'a>) -> Result<[u8; 4], &str> {
+    let abi = func_def(&stmt)?;
+    let input_types = abi.inputs.iter().map(|input| input.typ.to_string()).collect::<Vec<String>>();
+    let sig = format!("{}({})", abi.name, input_types.join(","));
 
-    let json = serde_json::to_string(&func).unwrap();
+    let mut keccak = Keccak::v256();
+    let mut selector = [0u8; 4];
+    keccak.update(sig.as_bytes());
+    keccak.finalize(&mut selector);
+    Ok(selector)
+}
 
-    assert_eq!(
-        json,
-        "{\"name\":\"foobar\",\"type\":\"function\",\"inputs\":[{\"name\":\"foo\",\"type\":\"uint256\"}],\"outputs\":[{\"name\":\"bar\",\"type\":\"uint256\"}]}",
-        "Serialized function not correct."
-    )
+#[cfg(test)]
+mod tests {
+    use crate::abi::{Function, FunctionType, Input, Output, VariableType, func_select};
+    use vyper_parser::parsers;
+
+    #[test]
+    fn test_function_serialize() {
+        let func = Function {
+            name: String::from("foobar"),
+            typ: FunctionType::Function,
+            inputs: vec![
+                Input {
+                    name: String::from("foo"),
+                    typ: VariableType::Uint256
+                }
+            ],
+            outputs: vec![
+                Output {
+                    name: String::from("bar"),
+                    typ: VariableType::Uint256
+                }
+            ]
+        };
+
+        let json = serde_json::to_string(&func).unwrap();
+
+        assert_eq!(
+            json,
+            "{\"name\":\"foobar\",\"type\":\"function\",\"inputs\":[{\"name\":\"foo\",\"type\":\"uint256\"}],\"outputs\":[{\"name\":\"bar\",\"type\":\"uint256\"}]}",
+            "Serialized function not correct."
+        )
+    }
+
+    #[test]
+    fn test_func_select() {
+        let toks = vyper_parser::get_parse_tokens("def foo(x: u256, y: u256) -> u256:\n   return x").unwrap();
+        let stmt = parsers::func_def(&toks[..]).unwrap().1.node;
+
+        let selector = hex::encode(func_select(&stmt).unwrap());
+
+        assert_eq!(
+            selector,
+            "04bc52f8",
+            "Function selector does not match."
+        );
+    }
 }
