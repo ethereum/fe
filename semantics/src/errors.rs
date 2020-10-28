2427271588cf2d5848fca3e9afb745b189888485
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2020-10-28 11:43:51 +0100

    Validate return values of function body
    Fixes #104

--- semantics/src/errors.rs
+++ semantics/src/errors.rs
@@ -3,8 +3,10 @@
 /// Errors for things that may arise in a valid Fe AST.
 #[derive(Debug, PartialEq)]
 pub enum SemanticError {
+    MissingReturn,
     NotAnExpression,
     UnassignableExpression,
     UndefinedValue,
+    UnexpectedReturn,
     TypeError,
 }
