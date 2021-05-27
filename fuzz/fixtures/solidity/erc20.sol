dc78242ca87017ba4bdc98657b8a267cbaeec123
SatyamAgrawal <satyam0499@gmail.com>
2021-05-27 17:18:27 +0530

    add event

--- fuzz/fixtures/solidity/erc20.sol
+++ fuzz/fixtures/solidity/erc20.sol
@@ -3,6 +3,20 @@ contract ERC20 {
 
     mapping (address => mapping (address => uint256)) private _allowances;
 
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
     uint256 private _totalSupply;
 
     string private _name;
