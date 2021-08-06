e362fe1d3fc2e30914ea52358cf122a086c3d3c8
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2021-08-06 13:49:29 +0200

    Reject certain code that should return but doesn't
    Fixes #497

--- crates/test-files/src/lib.rs
+++ crates/test-files/src/lib.rs
@@ -5,7 +5,7 @@ const FIXTURES: Dir = include_dir!("fixtures");
 pub fn fixture(path: &str) -> &'static str {
     FIXTURES
         .get_file(path)
-        .expect("bad fixture file path")
+        .unwrap_or_else(|| panic!("bad fixture file path {}", path))
         .contents_utf8()
         .expect("fixture file isn't utf8")
 }
@@ -13,6 +13,6 @@ pub fn fixture(path: &str) -> &'static str {
 pub fn fixture_bytes(path: &str) -> &'static [u8] {
     FIXTURES
         .get_file(path)
-        .expect("bad fixture file path")
+        .unwrap_or_else(|| panic!("bad fixture file path {}", path))
         .contents()
 }
