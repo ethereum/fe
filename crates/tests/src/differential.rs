2d0d0258da1e2e49062e4dc140f16212af4a497e
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2021-12-14 11:54:25 +0100

    Add storage / memory fuzzing test

--- crates/tests/src/differential.rs
+++ crates/tests/src/differential.rs
@@ -224,4 +224,40 @@ proptest! {
             harness.capture_call(&mut executor, "negate", &[int_token(val.into())]).assert_perfomed_equal();
         });
     }
+
+    #[test]
+    #[ignore]
+    fn storage_and_memory(my_num in 0u64..=100000,
+                          my_num2 in 0u8..=255, my_bool in any::<bool>(),
+                          my_str in "[0-9]{20}",
+                          my_long_string in ".{0,40}",
+                          my_num3 in -128i8..=127i8) {
+        with_executor(&|mut executor| {
+
+
+            let harness = DualHarness::from_fixture(&mut executor, "storage_and_memory", "Foo", &[]);
+
+            let data = ethabi::Token::Tuple(vec![
+                uint_token(my_num),
+                uint_token(my_num2.into()),
+                bool_token(my_bool),
+                address_token(&my_str),
+                int_token(my_num3.into())
+            ]);
+
+            harness.capture_call(&mut executor, "set_data", &[data]).assert_any_success_with_equal_return_data().assert_fe_max_percentage_more_gas(200);
+            harness.capture_call(&mut executor, "get_data", &[]).assert_perfomed_equal().assert_fe_max_percentage_more_gas(150);
+
+            harness.capture_call(&mut executor, "set_item", &[uint_token(my_num2.into()), int_token(my_num3.into())]).assert_any_success_or_revert_with_equal_return_data();
+            // Waiting on a fix for https://github.com/ethereum/fe/pull/581
+            //harness.capture_call(&mut executor, "get_items", &[]).assert_perfomed_equal();
+
+            harness.capture_call(&mut executor, "set_string", &[string_token(&my_long_string)]).assert_any_success_with_equal_return_data();
+            harness.capture_call(&mut executor, "get_string", &[]).assert_perfomed_equal();
+
+            harness.capture_call(&mut executor, "set_range", &[uint_token(my_num2.into()), uint_token(my_num)]).assert_any_success_or_revert_with_equal_return_data();
+            harness.capture_call(&mut executor, "get_range", &[]).assert_perfomed_equal().assert_any_success_or_revert_with_equal_return_data();
+
+        });
+    }
 }
