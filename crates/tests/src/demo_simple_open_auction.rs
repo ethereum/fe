5162737a429499dcf2e56780398a45c3f4904c6d
Vu Vo <vu.voth@gmail.com>
2022-05-14 23:31:48 +0700

    fix rustfmt

--- crates/tests/src/demo_simple_open_auction.rs
+++ crates/tests/src/demo_simple_open_auction.rs
@@ -1,7 +1,7 @@
 #![cfg(feature = "solc-backend")]
-use primitive_types::{U256, H256};
 use fe_compiler_test_utils::*;
 use insta::assert_snapshot;
+use primitive_types::U256;
 
 #[test]
 fn simple_open_auction() {
@@ -22,23 +22,28 @@ fn simple_open_auction() {
 
         // alice bid first
         harness.caller = alice.clone().into_address().unwrap();
-        harness.value = U256::from(10000); 
+        harness.value = U256::from(10000);
 
         harness.test_function(&mut executor, "bid", &[], None);
         executor.state_mut().deposit(harness.address, harness.value);
-        
+
         // bob bid second
         harness.caller = bob.clone().into_address().unwrap();
-        harness.value = U256::from(100000); 
+        harness.value = U256::from(100000);
 
         harness.test_function(&mut executor, "bid", &[], None);
         executor.state_mut().deposit(harness.address, harness.value);
 
         // alice bib again but fail b/c "Bid not high enough"
         harness.caller = alice.clone().into_address().unwrap();
-        harness.value = U256::from(10000); 
+        harness.value = U256::from(10000);
 
-        harness.test_function_reverts(&mut executor, "bid", &[], &encode_error_reason("Bid not high enough"));
+        harness.test_function_reverts(
+            &mut executor,
+            "bid",
+            &[],
+            &encode_error_reason("Bid not high enough"),
+        );
 
         // alice withdraw money
         harness.caller = alice.clone().into_address().unwrap();
@@ -49,27 +54,20 @@ fn simple_open_auction() {
         harness.caller = charlie.clone().into_address().unwrap();
         harness.value = U256::zero();
         harness.test_function(&mut executor, "withdraw", &[], Some(&bool_token(true)));
-        
 
         // verify event emitted
         harness.events_emitted(
-            executor, 
+            executor,
             &[
                 (
                     "HighestBidIncreased",
-                    &[
-                        alice,
-                        uint_token_from_dec_str("10000"),
-                    ]
+                    &[alice, uint_token_from_dec_str("10000")],
                 ),
                 (
                     "HighestBidIncreased",
-                    &[
-                        bob,
-                        uint_token_from_dec_str("100000"),
-                    ]
-                )
-            ]
+                    &[bob, uint_token_from_dec_str("100000")],
+                ),
+            ],
         );
 
         assert_harness_gas_report!(harness);
--- crates/tests/src/lib.rs
+++ crates/tests/src/lib.rs
@@ -5,9 +5,9 @@ mod demo_erc20;
 #[cfg(test)]
 mod demo_guestbook;
 #[cfg(test)]
-mod demo_uniswap;
-#[cfg(test)]
 mod demo_simple_open_auction;
+#[cfg(test)]
+mod demo_uniswap;
 #[cfg(test)]
 mod differential;
 #[cfg(test)]
