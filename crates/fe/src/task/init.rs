6f399b47b8a9f860956c174ee37d145a025b1fe8
Vu Vo <vu.voth@gmail.com>
2022-06-04 12:29:51 +0700

    fmt

--- crates/fe/src/task/init.rs
+++ crates/fe/src/task/init.rs
@@ -1,6 +1,6 @@
 use clap::Args;
-use std::{fs, path::Path};
 use include_dir::{include_dir, Dir};
+use std::{fs, path::Path};
 
 const TEMPLATE: Dir = include_dir!("$CARGO_MANIFEST_DIR/src/template/src");
 
@@ -12,16 +12,19 @@ pub struct InitArgs {
 fn create_project(p: &Path) {
     for file in TEMPLATE.entries() {
         let f = file.as_file().unwrap();
-        fs::write(p.join(f.path()),f.contents()).unwrap();
+        fs::write(p.join(f.path()), f.contents()).unwrap();
     }
 }
 
 pub fn init(args: InitArgs) {
     if Path::new(&args.name).exists() {
-        eprintln!("Project  {} does exist. Please choice another name!", args.name);
+        eprintln!(
+            "Project  {} does exist. Please choice another name!",
+            args.name
+        );
         std::process::exit(1)
     }
-    
+
     let p = Path::new(&args.name).join("src");
 
     match fs::create_dir_all(p.as_path()) {
