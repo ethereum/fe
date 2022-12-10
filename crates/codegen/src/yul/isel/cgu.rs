054aaed8733d2c7eb4f08b3bfc6a466ac7964035
Yoshitomo Nakanishi <yurayura.rounin.3@gmail.com>
2022-12-10 13:03:21 +0100

    Add runtime functions collection

--- crates/codegen/src/yul/isel/cgu.rs
+++ crates/codegen/src/yul/isel/cgu.rs
@@ -1,5 +1,5 @@
 use fe_mir::ir::FunctionSigId;
-use indexmap::IndexMap;
+use indexmap::{IndexMap, IndexSet};
 use yultsur::yul;
 
 use crate::{
@@ -11,59 +11,63 @@ use crate::{
 use super::lower_function;
 
 pub(super) fn compile_cgu(db: &dyn CodegenDb, cgu: CodegenUnit) -> CompiledCgu {
-    let mut ctx = CguContext::default();
+    let mut runtime_func_deps = IndexMap::default();
     let mut functions = IndexMap::with_capacity(cgu.functions.len());
+    let mut runtime_funcs = IndexMap::default();
+    let mut constants = IndexSet::default();
+
     for func in cgu.functions {
+        let mut ctx = CguLowerContext::default();
         let sig = func.sig;
         let yul_func = lower_function(db, &mut ctx, func);
+        for runtime_func in ctx.runtime.collect_definitions() {
+            runtime_func_deps
+                .entry(sig)
+                .or_insert_with(Vec::new)
+                .push(runtime_func.name.identifier.clone());
+            runtime_funcs.insert(runtime_func.name.identifier.clone(), runtime_func);
+            constants.extend(ctx.string_constants);
+        }
         functions.insert(sig, yul_func);
-    }
-
-    let mut runtime_functions = IndexMap::default();
-
-    for func in ctx.runtime.collect_definitions() {
-        runtime_functions.insert(func.name.identifier.clone(), func);
     }
 
     CompiledCgu {
         functions,
-        constants: ctx.string_constants,
-        runtime_functions,
+        constants,
+        runtime_funcs,
+        runtime_func_deps,
     }
 }
 
+#[derive(Default)]
 pub(super) struct CompiledCgu {
     functions: IndexMap<FunctionSigId, yul::FunctionDefinition>,
-    constants: IndexMap<String, yul::Data>,
-    runtime_functions: IndexMap<String, yul::FunctionDefinition>,
+    constants: IndexSet<yul::Data>,
+    runtime_funcs: IndexMap<String, yul::FunctionDefinition>,
+    runtime_func_deps: IndexMap<FunctionSigId, Vec<String>>,
 }
 
-pub struct CguContext {
+pub(super) struct CguLowerContext {
     pub(super) runtime: Box<dyn RuntimeProvider>,
-    pub(super) string_constants: IndexMap<String, yul::Data>,
+    pub(super) string_constants: IndexSet<yul::Data>,
 }
 
-impl CguContext {
-    pub fn add_string_constant(&mut self, db: &dyn CodegenDb, s: &str) {
-        let symbol_name = db.codegen_constant_string_symbol_name(s.into());
-        if self.string_constants.contains_key(symbol_name.as_ref()) {
-            return;
+impl Default for CguLowerContext {
+    fn default() -> Self {
+        Self {
+            runtime: Box::new(DefaultRuntimeProvider::default()),
+            string_constants: IndexSet::default(),
         }
+    }
+}
 
+impl CguLowerContext {
+    pub(super) fn add_string_constant(&mut self, db: &dyn CodegenDb, s: &str) {
+        let symbol_name = db.codegen_constant_string_symbol_name(s.into());
         let data = yul::Data {
             name: symbol_name.as_ref().clone(),
             value: s.to_string(),
         };
-        self.string_constants
-            .insert(symbol_name.as_ref().clone(), data);
-    }
-}
-
-impl Default for CguContext {
-    fn default() -> Self {
-        Self {
-            runtime: Box::new(DefaultRuntimeProvider::default()),
-            string_constants: IndexMap::default(),
-        }
+        self.string_constants.insert(data);
     }
 }
