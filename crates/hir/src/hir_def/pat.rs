9fe4f53b0ccb2669cf19f417d8e7bb8c157904cc
Yoshitomo Nakanishi <yurayura.rounin.3@gmail.com>
2023-03-16 00:36:08 +0100

    Add HIR `pat` def

---
+++ crates/hir/src/hir_def/pat.rs
@@ -0,0 +1,27 @@
+use cranelift_entity::entity_impl;
+
+use super::{IdentId, LitKind, PathId};
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum Pat {
+    WildCard,
+    Rest,
+    Lit(LitKind),
+    Tuple(Vec<PatId>),
+    Path(PathId),
+    PathTuple(PathId, Vec<PatId>),
+    Record(PathId, Vec<RecordPatField>),
+    Or(PatId, PatId),
+
+    Invalid,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct PatId(u32);
+entity_impl!(PatId);
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct RecordPatField {
+    pub label: Option<IdentId>,
+    pub pat: PatId,
+}
