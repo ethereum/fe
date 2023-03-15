85fc0b8c38a59f9864413c5669a012f5f7832a77
Yoshitomo Nakanishi <yurayura.rounin.3@gmail.com>
2023-03-16 00:35:39 +0100

    Add HIR `body` def

---
+++ crates/hir/src/hir_def/body.rs
@@ -0,0 +1,29 @@
+use cranelift_entity::{PrimaryMap, SecondaryMap};
+use fe_parser2::ast::{self, Stmt};
+
+use crate::span::HirOrigin;
+
+use super::{Expr, ExprId, Pat, PatId, StmtId};
+
+#[salsa::tracked]
+pub struct Body {
+    #[id]
+    pub kind: BodyKind,
+
+    pub stmts: PrimaryMap<StmtId, Stmt>,
+    pub exprs: PrimaryMap<ExprId, Expr>,
+    pub pats: PrimaryMap<PatId, Pat>,
+
+    pub(crate) stmt_source_map: SecondaryMap<StmtId, HirOrigin<ast::AstPtr<ast::Stmt>>>,
+    pub(crate) expr_source_map: SecondaryMap<ExprId, HirOrigin<ast::AstPtr<ast::Expr>>>,
+    pub(crate) pat_source_map: SecondaryMap<ExprId, HirOrigin<ast::AstPtr<ast::Expr>>>,
+    pub(crate) ast: HirOrigin<ast::AstPtr<ast::Expr>>,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum BodyKind {
+    /// This is a body appearing in a item, e.g., a function or const item.
+    DefBlock(super::ItemKind),
+    /// This is a body appearing in array types or
+    NamelessConst,
+}
