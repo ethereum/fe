eadd0438dad394c89820f1b7c0fd58165a1cff28
Yoshitomo Nakanishi <yurayura.rounin.3@gmail.com>
2023-03-16 03:04:27 +0100

    Define `IngotId` and `FileId`

--- crates/hir/src/span/mod.rs
+++ crates/hir/src/span/mod.rs
@@ -1,4 +1,15 @@
+use std::path::PathBuf;
+
 use fe_parser2::{ast::AstPtr, SyntaxNode};
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct HirOrigin<T>
+where
+    T: Send + Clone + PartialEq + Eq + std::fmt::Debug + std::hash::Hash,
+{
+    pub file_id: FileId,
+    pub kind: HirOriginKind<T>,
+}
 
 /// This enum represents the origin of the HIR node.
 /// The origin has three possible kinds.
@@ -7,7 +18,7 @@ use fe_parser2::{ast::AstPtr, SyntaxNode};
 /// 3. `Desugared` is used for nodes that are created by the compiler and not
 // TODO: Change the visibility to `pub(crate)` when https://github.com/salsa-rs/salsa/issues/437 is resolved.
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub enum HirOrigin<T>
+pub enum HirOriginKind<T>
 where
     T: Send + Clone + PartialEq + Eq + std::fmt::Debug + std::hash::Hash,
 {
@@ -29,4 +40,36 @@ pub enum DesugaredOrigin {
     /// The HIR node is the result of desugaring an augmented assignment
     /// statement.
     AugAssign(AstPtr<fe_parser2::ast::AugAssignStmt>),
+}
+
+/// This enum represents the file
+#[salsa::interned]
+pub struct FileId {
+    /// A ingot id which the file belongs to.
+    ingot: IngotId,
+    /// A relative path from the ingot root.
+    path: PathBuf,
+}
+
+#[salsa::interned]
+pub struct IngotId {
+    /// A full path to the ingot root.
+    path: PathBuf,
+    kind: IngotKind,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum IngotKind {
+    /// A standalone ingot is a dummy ingot when the compiler is invoked
+    /// directly on a file.
+    StandAlone,
+
+    /// A local ingot which is the current ingot being compiled.
+    Local,
+
+    /// An external ingot which is depended on by the current ingot.
+    External,
+
+    /// A std ingot.
+    Std,
 }
