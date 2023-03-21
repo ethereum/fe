3feec5bfde5e9337384acdb218d61f61d2bbc3a2
Yoshitomo Nakanishi <yurayura.rounin.3@gmail.com>
2023-03-21 02:50:41 +0100

    Add HIR lower for `Body`

--- crates/hir/src/lower/body.rs
+++ crates/hir/src/lower/body.rs
@@ -1,8 +1,10 @@
-use cranelift_entity::{PrimaryMap, SecondaryMap};
 use fe_parser2::ast;
 
 use crate::{
-    hir_def::{Body, Expr, ExprId, ItemKind, MaybeInvalid, Pat, PatId, Stmt, StmtId},
+    hir_def::{
+        Body, BodyKind, BodyNodeMap, BodySourceMap, Expr, ExprId, ItemKind, MaybeInvalid, Pat,
+        PatId, Stmt, StmtId,
+    },
     span::{FileId, HirOrigin, HirOriginKind},
     HirDb,
 };
@@ -14,24 +16,28 @@ impl Body {
         parent_item: ItemKind,
         ast: ast::Expr,
     ) -> Self {
-        todo!()
+        let mut ctxt = BodyCtxt::new(db, fid);
+        Expr::push_to_body(&mut ctxt, ast.clone());
+        ctxt.build(BodyKind::ItemBody(parent_item), HirOrigin::raw(fid, &ast))
     }
 
     pub(crate) fn nameless_from_ast(db: &dyn HirDb, fid: FileId, ast: ast::Expr) -> Self {
-        todo!()
+        let mut ctxt = BodyCtxt::new(db, fid);
+        Expr::push_to_body(&mut ctxt, ast.clone());
+        ctxt.build(BodyKind::NamelessConst, HirOrigin::raw(fid, &ast))
     }
 }
 
 pub(super) struct BodyCtxt<'db> {
-    pub(super) stmts: PrimaryMap<StmtId, MaybeInvalid<Stmt>>,
-    pub(super) exprs: PrimaryMap<ExprId, MaybeInvalid<Expr>>,
-    pub(super) pats: PrimaryMap<PatId, MaybeInvalid<Pat>>,
+    pub(super) stmts: BodyNodeMap<StmtId, MaybeInvalid<Stmt>>,
+    pub(super) exprs: BodyNodeMap<ExprId, MaybeInvalid<Expr>>,
+    pub(super) pats: BodyNodeMap<PatId, MaybeInvalid<Pat>>,
     pub(super) db: &'db dyn HirDb,
     pub(super) fid: FileId,
 
-    stmt_source_map: SecondaryMap<StmtId, HirOrigin<ast::Stmt>>,
-    expr_source_map: SecondaryMap<ExprId, HirOrigin<ast::Expr>>,
-    pat_source_map: SecondaryMap<PatId, HirOrigin<ast::Pat>>,
+    stmt_source_map: BodySourceMap<StmtId, ast::Stmt>,
+    expr_source_map: BodySourceMap<ExprId, ast::Expr>,
+    pat_source_map: BodySourceMap<PatId, ast::Pat>,
 }
 impl<'db> BodyCtxt<'db> {
     pub(super) fn push_expr(&mut self, expr: Expr, origin: HirOriginKind<ast::Expr>) -> ExprId {
@@ -68,5 +74,32 @@ impl<'db> BodyCtxt<'db> {
         let pat_id = self.pats.push(None.into());
         self.pat_source_map[pat_id] = HirOrigin::none(self.fid);
         pat_id
+    }
+
+    fn new(db: &'db dyn HirDb, fid: FileId) -> Self {
+        Self {
+            stmts: BodyNodeMap::new(),
+            exprs: BodyNodeMap::new(),
+            pats: BodyNodeMap::new(),
+            db,
+            fid,
+            stmt_source_map: BodySourceMap::new(),
+            expr_source_map: BodySourceMap::new(),
+            pat_source_map: BodySourceMap::new(),
+        }
+    }
+
+    fn build(self, kind: BodyKind, origin: HirOrigin<ast::Expr>) -> Body {
+        Body::new(
+            self.db,
+            kind,
+            self.stmts,
+            self.exprs,
+            self.pats,
+            self.stmt_source_map,
+            self.expr_source_map,
+            self.pat_source_map,
+            origin,
+        )
     }
 }
