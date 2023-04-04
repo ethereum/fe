e3cf853867a6a9067a64fb4375b5d9f8f7eae151
Yoshitomo Nakanishi <yurayura.rounin.3@gmail.com>
2023-04-04 23:45:39 +0200

    Add lazy span for Path

--- crates/hir/src/span/path.rs
+++ crates/hir/src/span/path.rs
@@ -1,3 +1,20 @@
+use parser::{
+    ast::{self, prelude::*},
+    SyntaxNode,
+};
+
 use super::define_lazy_span_item;
 
 define_lazy_span_item!(LazyPathSpan);
+impl LazyPathSpan {
+    pub fn segment(&self, idx: usize) -> LazyPathSegmentSpan {
+        let transition = move |node: SyntaxNode| {
+            ast::RecordFieldList::cast(node)
+                .and_then(|f| f.into_iter().nth(idx))
+                .map(|n| n.syntax().clone().into())
+        };
+        LazyPathSegmentSpan(self.0.push_state(std::sync::Arc::new(transition)))
+    }
+}
+
+define_lazy_span_item!(LazyPathSegmentSpan);
