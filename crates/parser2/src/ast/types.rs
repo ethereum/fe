15783cd74248c80384c661d45ddad9cde6630ec4
Yoshitomo Nakanishi <yurayura.rounin.3@gmail.com>
2023-04-05 17:29:42 +0200

    Add lazy span for types

--- crates/parser2/src/ast/types.rs
+++ crates/parser2/src/ast/types.rs
@@ -79,6 +79,14 @@ ast_node! {
     IntoIterator<Item=Type>,
 }
 impl TupleType {
+    pub fn l_paren(&self) -> Option<SyntaxToken> {
+        support::token(self.syntax(), SK::LParen)
+    }
+
+    pub fn r_paren(&self) -> Option<SyntaxToken> {
+        support::token(self.syntax(), SK::RParen)
+    }
+
     /// Returns the types in the tuple.
     pub fn elem_tys(&self) -> AstChildren<Type> {
         support::children(self.syntax())
@@ -92,6 +100,13 @@ ast_node! {
     SK::ArrayType,
 }
 impl ArrayType {
+    pub fn l_bracket(&self) -> Option<SyntaxToken> {
+        support::token(self.syntax(), SK::LBracket)
+    }
+
+    pub fn r_bracket(&self) -> Option<SyntaxToken> {
+        support::token(self.syntax(), SK::LBracket)
+    }
     /// Returns the type of the array elements.
     pub fn elem_ty(&self) -> Option<Type> {
         support::child(self.syntax())
