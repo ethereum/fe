3d5b1b1f15a2403839ba6712acb20cb53fca9620
Yoshitomo Nakanishi <yurayura.rounin.3@gmail.com>
2023-06-27 12:19:37 +0200

    Add simple analysis pass manager

--- crates/hir/src/analysis_pass.rs
+++ crates/hir/src/analysis_pass.rs
@@ -14,3 +14,41 @@ pub trait FuncAnalysisPass {
 pub trait ModuleAnalysisPass {
     fn run_on_module(&mut self, top_mod: TopLevelMod) -> Vec<Box<dyn DiagnosticVoucher>>;
 }
+
+pub struct AnalysisPassManager<'db> {
+    module_passes: Vec<Box<dyn ModuleAnalysisPass + 'db>>,
+    func_passes: Vec<Box<dyn FuncAnalysisPass + 'db>>,
+}
+
+impl<'db> AnalysisPassManager<'db> {
+    pub fn new() -> Self {
+        Self {
+            module_passes: vec![],
+            func_passes: vec![],
+        }
+    }
+
+    pub fn add_module_pass(&mut self, pass: Box<dyn ModuleAnalysisPass + 'db>) {
+        self.module_passes.push(pass);
+    }
+
+    pub fn add_func_pass(&mut self, pass: Box<dyn FuncAnalysisPass + 'db>) {
+        self.func_passes.push(pass);
+    }
+
+    pub fn run_on_module(&mut self, top_mod: TopLevelMod) -> Vec<Box<dyn DiagnosticVoucher>> {
+        let mut diags = vec![];
+        for pass in self.module_passes.iter_mut() {
+            diags.extend(pass.run_on_module(top_mod.clone()));
+        }
+        diags
+    }
+
+    pub fn run_on_func(&mut self, func: Func) -> Vec<Box<dyn DiagnosticVoucher>> {
+        let mut diags = vec![];
+        for pass in self.func_passes.iter_mut() {
+            diags.extend(pass.run_on_func(func.clone()));
+        }
+        diags
+    }
+}
