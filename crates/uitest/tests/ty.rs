3b17efbb8a7a87d634c212499f731a15433109b0
Yoshitomo Nakanishi <yurayura.rounin.3@gmail.com>
2023-10-10 18:26:35 +0200

    Opt out cycle-related tests from wasm tests: ref https://github.com/ethereum/fe/issues/939

--- crates/uitest/tests/ty.rs
+++ crates/uitest/tests/ty.rs
@@ -35,33 +35,42 @@ mod wasm {
     use super::*;
     use wasm_bindgen_test::wasm_bindgen_test;
 
-    #[dir_test(
-    dir: "$CARGO_MANIFEST_DIR/fixtures/ty/def",
-    glob: "*.fe",
-    postfix: "wasm"
-    )]
-    #[dir_test_attr(
-        #[wasm_bindgen_test]
-    )]
-    fn run_ty_def(fixture: Fixture<&str>) {
-        let mut driver = DriverDataBase::default();
-        let path = Path::new(fixture.path());
-        let top_mod = driver.top_mod_from_file(path, fixture.content());
-        driver.run_on_top_mod(top_mod);
+    mod def {
+        use super::*;
+
+        // TODO: we opt out the tests for type/alias/trait-infinite recursion checks. See https://github.com/ethereum/fe/issues/939 for more details.
+        #[dir_test(
+        dir: "$CARGO_MANIFEST_DIR/fixtures/ty/def",
+        glob: "*[!_cycle].fe",
+        postfix: "wasm"
+        )]
+        #[dir_test_attr(
+            #[wasm_bindgen_test]
+        )]
+        fn run_ty_def(fixture: Fixture<&str>) {
+            let mut driver = DriverDataBase::default();
+            let path = Path::new(fixture.path());
+            let top_mod = driver.top_mod_from_file(path, fixture.content());
+            driver.run_on_top_mod(top_mod);
+        }
     }
 
-    #[dir_test(
-    dir: "$CARGO_MANIFEST_DIR/fixtures/ty/trait_bound",
-    glob: "*.fe",
-    postfix: "wasm"
-    )]
-    #[dir_test_attr(
-        #[wasm_bindgen_test]
-    )]
-    fn run_trait_bound(fixture: Fixture<&str>) {
-        let mut driver = DriverDataBase::default();
-        let path = Path::new(fixture.path());
-        let top_mod = driver.top_mod_from_file(path, fixture.content());
-        driver.run_on_top_mod(top_mod);
+    mod trait_bound {
+        use super::*;
+
+        #[dir_test(
+        dir: "$CARGO_MANIFEST_DIR/fixtures/ty/trait_bound",
+        glob: "*.fe",
+        postfix: "wasm"
+        )]
+        #[dir_test_attr(
+            #[wasm_bindgen_test]
+        )]
+        fn run_trait_bound(fixture: Fixture<&str>) {
+            let mut driver = DriverDataBase::default();
+            let path = Path::new(fixture.path());
+            let top_mod = driver.top_mod_from_file(path, fixture.content());
+            driver.run_on_top_mod(top_mod);
+        }
     }
 }
