17ccf90501098664e727b563b8f97cfe30dbb383
Micah <micahscopes@gmail.com>
2023-12-07 16:54:32 -0500

    LSP workspace change proof of concept

--- crates/language-server/src/server.rs
+++ crates/language-server/src/server.rs
@@ -13,6 +13,57 @@ fn server_capabilities() -> ServerCapabilities {
         // goto definition
         definition_provider: Some(lsp_types::OneOf::Left(true)),
         // support for workspace add/remove changes
+        workspace: Some(lsp_types::WorkspaceServerCapabilities {
+            workspace_folders: Some(lsp_types::WorkspaceFoldersServerCapabilities {
+                supported: Some(true),
+                change_notifications: Some(lsp_types::OneOf::Left(true)),
+            }),
+            file_operations: Some(lsp_types::WorkspaceFileOperationsServerCapabilities {
+                did_create: Some(lsp_types::FileOperationRegistrationOptions {
+                    filters: vec![lsp_types::FileOperationFilter {
+                        scheme: Some(String::from("file")),
+                        pattern: lsp_types::FileOperationPattern {
+                            glob: String::from("**/*"),
+                            options: None,
+                            // options: Some(lsp_types::FileOperationPatternOptions {
+                            //     ignore_case: Some(true),
+                            // }),
+                            matches: None,
+                        },
+                    }],
+                }),
+                did_rename: Some(lsp_types::FileOperationRegistrationOptions {
+                    filters: vec![lsp_types::FileOperationFilter {
+                        scheme: Some(String::from("file")),
+                        pattern: lsp_types::FileOperationPattern {
+                            glob: String::from("**/*"),
+                            options: None,
+                            // options: Some(lsp_types::FileOperationPatternOptions {
+                            //     ignore_case: Some(true),
+                            // }),
+                            matches: None,
+                        },
+                    }],
+                }),
+                did_delete: Some(lsp_types::FileOperationRegistrationOptions {
+                    filters: vec![lsp_types::FileOperationFilter {
+                        scheme: Some(String::from("file")),
+                        pattern: lsp_types::FileOperationPattern {
+                            glob: String::from("**/*"),
+                            options: None,
+                            // options: Some(lsp_types::FileOperationPatternOptions {
+                            //     ignore_case: Some(true),
+                            // }),
+                            matches: None,
+                        },
+                    }],
+                }),
+                will_create: None,
+                will_rename: None,
+                will_delete: None,
+                })
+            }),
+            // ..Default::default()
         ..Default::default()
     }
 }
