2a1b6d3007ebc460b9074f4b81b70471b85bc296
saifalkatout <saif.katout@hotmail.com>
2024-02-12 22:44:22 +0400

    WASM test

--- crates/hir/src/lower/path.rs
+++ crates/hir/src/lower/path.rs
@@ -30,9 +30,5 @@ impl PathId {
         ast.map(|ast| Self::lower_ast(ctxt, ast)).into()
     }
 
-    pub(super) fn from_token(ctxt: &mut FileLowerCtxt<'_>, ast: SyntaxToken) -> Self {
-        let ident_id = IdentId::new(ctxt.db(), ast.text().to_string());
-        let seg = vec![Partial::Present(ident_id)];
-        Self::new(ctxt.db(), seg)
-    }
+   
 }
