eb2cb3f3411332e228083d70182b948c7c7fd5f7
Yoshitomo Nakanishi <yurayura.rounin.3@gmail.com>
2024-02-26 23:32:55 +0100

    ty_check: field access expresssion

--- crates/hir-analysis/src/ty/visitor.rs
+++ crates/hir-analysis/src/ty/visitor.rs
@@ -61,7 +61,7 @@ where
         TyData::TyBase(ty_con) => visitor.visit_ty_base(db, ty_con),
 
         TyData::ConstTy(const_ty) => visitor.visit_const_ty(db, const_ty),
-        
+
         TyData::Bot => {}
 
         TyData::Invalid(cause) => visitor.visit_invalid(db, cause),
