843ed9a3572cf651cc49149335494e6a97ad5ade
Micah <micahscopes@gmail.com>
2024-02-29 23:05:40 -0600

    stream handlers for LSP functionality

--- crates/language-server/src/backend.rs
+++ crates/language-server/src/backend.rs
@@ -1,3 +1,6 @@
+use crate::workspace::SyncableIngotFileContext;
+use futures::TryStreamExt;
+use lsp_types::TextDocumentItem;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 
@@ -6,8 +9,10 @@ use tokio::sync::Mutex;
 use crate::capabilities::server_capabilities;
 use crate::db::LanguageServerDatabase;
 
+use crate::diagnostics::get_diagnostics;
+use crate::globals::LANGUAGE_ID;
 use crate::language_server::Server;
-use crate::workspace::Workspace;
+use crate::workspace::{IngotFileContext, SyncableInputFile, Workspace};
 
 use log::info;
 
@@ -16,30 +21,36 @@ use tokio_stream::StreamExt;
 use tower_lsp::Client;
 
 pub struct Backend<'a> {
-    // pub(crate) client: &'a Client,
     pub(crate) server: &'a Server,
-    pub(crate) client: Arc<Mutex<Client>>,
-    pub(crate) db: LanguageServerDatabase,
-    pub(crate) workspace: Workspace,
+    pub(crate) client: Arc<tokio::sync::Mutex<Client>>,
+    pub(crate) db: Arc<Mutex<LanguageServerDatabase>>,
+    pub(crate) workspace: Arc<Mutex<Workspace>>,
+    runtime: tokio::runtime::Runtime,
 }
 
 impl<'a> Backend<'a> {
-    pub fn new(client: Arc<Mutex<Client>>, server: &'a Server) -> Self {
-        let workspace = Workspace::default();
-        let db = LanguageServerDatabase::default();
+    pub fn new(client: Arc<tokio::sync::Mutex<Client>>, server: &'a Server) -> Self {
+        let workspace = Arc::new(Mutex::new(Workspace::default()));
+        let db = Arc::new(Mutex::new(LanguageServerDatabase::default()));
+        let runtime = tokio::runtime::Runtime::new().unwrap();
         let backend = Self {
             server,
             client,
             db,
             workspace,
+            runtime,
         };
 
         backend
     }
-    pub fn setup_streams(mut self) {
-        let mut stream = BroadcastStream::new(self.server.dispatch.subscribe_initialize());
-        tokio::spawn(async move {
-            while let Some(result) = stream.next().await {
+    pub fn setup_streams(self) {
+        let db = self.db.clone();
+        let workspace = self.workspace.clone();
+        let client = self.client.clone();
+        let mut initialized_stream =
+            BroadcastStream::new(self.server.dispatch.subscribe_initialize());
+        self.runtime.spawn(async move {
+            while let Some(result) = initialized_stream.next().await {
                 if let Ok((initialization_params, responder)) = result {
                     info!("initializing language server: {:?}", initialization_params);
                     // setup workspace
@@ -51,8 +62,10 @@ impl<'a> Backend<'a> {
                             version: Some(String::from(env!("CARGO_PKG_VERSION"))),
                         }),
                     };
-                    let _ = self.workspace.set_workspace_root(
-                        &mut self.db,
+                    let db = &mut self.db.lock().await;
+                    let workspace = &mut self.workspace.lock().await;
+                    let _ = workspace.set_workspace_root(
+                        db,
                         initialization_params
                             .root_uri
                             .unwrap()
@@ -65,5 +78,163 @@ impl<'a> Backend<'a> {
                 }
             }
         });
+
+        let mut shutdown_stream = BroadcastStream::new(self.server.dispatch.subscribe_shutdown());
+        self.runtime.spawn(async move {
+            while let Some(result) = shutdown_stream.next().await {
+                if let Ok((_, responder)) = result {
+                    info!("shutting down language server");
+                    responder.respond(Ok(()));
+                }
+            }
+        });
+
+        let did_open_stream = BroadcastStream::new(self.server.dispatch.subscribe_did_open());
+        let did_change_stream = BroadcastStream::new(self.server.dispatch.subscribe_did_change());
+
+        let mut change_stream = tokio_stream::StreamExt::merge(
+            did_open_stream.map_ok(|params| TextDocumentItem {
+                uri: params.text_document.uri,
+                language_id: LANGUAGE_ID.to_string(),
+                version: params.text_document.version,
+                text: params.text_document.text,
+            }),
+            did_change_stream.map_ok(|params| TextDocumentItem {
+                uri: params.text_document.uri,
+                language_id: LANGUAGE_ID.to_string(),
+                version: params.text_document.version,
+                text: params.content_changes[0].text.clone(),
+            }),
+        );
+
+        let workspace_clone = workspace.clone();
+        let client_clone = client.clone();
+        let db_clone = db.clone();
+        self.runtime.spawn(async move {
+            let workspace = &mut workspace_clone.lock().await;
+            let client = &mut client_clone.lock().await;
+            let db = &mut db_clone.lock().await;
+            while let Some(Ok(doc)) = change_stream.next().await {
+                on_change(client, workspace, db, doc).await;
+            }
+        });
+
+        let workspace_clone = workspace.clone();
+        let client_clone = client.clone();
+        let db_clone = db.clone();
+        let mut did_close_stream = BroadcastStream::new(self.server.dispatch.subscribe_did_close());
+        self.runtime.spawn(async move {
+            let workspace = &mut workspace_clone.lock().await;
+            let client = &mut client_clone.lock().await;
+            let db = &mut db_clone.lock().await;
+            while let Some(Ok(params)) = did_close_stream.next().await {
+                let input = workspace
+                    .input_from_file_path(
+                        db,
+                        params
+                            .text_document
+                            .uri
+                            .to_file_path()
+                            .unwrap()
+                            .to_str()
+                            .unwrap(),
+                    )
+                    .unwrap();
+                let _ = input.sync(db, None);
+            }
+        });
+
+        let workspace_clone = workspace.clone();
+        let client_clone = client.clone();
+        let db_clone = db.clone();
+        let mut did_change_watched_files_stream =
+            BroadcastStream::new(self.server.dispatch.subscribe_did_change_watched_files());
+        self.runtime.spawn(async move {
+            let workspace = &mut workspace_clone.lock().await;
+            let client = &mut client_clone.lock().await;
+            let db = &mut db_clone.lock().await;
+            
+            while let Some(Ok(params)) = did_change_watched_files_stream.next().await {
+                let changes = params.changes;
+                for change in changes {
+                    let uri = change.uri;
+                    let path = uri.to_file_path().unwrap();
+
+                    match change.typ {
+                        lsp_types::FileChangeType::CREATED => {
+                            // TODO: handle this more carefully!
+                            // this is inefficient, a hack for now
+                            let _ = workspace.sync(db);
+                            let input = workspace
+                                .input_from_file_path(db, path.to_str().unwrap())
+                                .unwrap();
+                            let _ = input.sync(db, None);
+                        }
+                        lsp_types::FileChangeType::CHANGED => {
+                            let input = workspace
+                                .input_from_file_path(db, path.to_str().unwrap())
+                                .unwrap();
+                            let _ = input.sync(db, None);
+                        }
+                        lsp_types::FileChangeType::DELETED => {
+                            // TODO: handle this more carefully!
+                            // this is inefficient, a hack for now
+                            let _ = workspace.sync(db);
+                        }
+                        _ => {}
+                    }
+                    // collect diagnostics for the file
+                    if change.typ != lsp_types::FileChangeType::DELETED {
+                        let text = std::fs::read_to_string(path).unwrap();
+                        on_change(
+                            client,
+                            workspace,
+                            db,
+                            TextDocumentItem {
+                                uri: uri.clone(),
+                                language_id: LANGUAGE_ID.to_string(),
+                                version: 0,
+                                text,
+                            },
+                        )
+                        .await;
+                    }
+                }
+            }
+        });
     }
+}
+
+async fn on_change(
+    client: &mut Client,
+    workspace: &mut Workspace,
+    db: &mut LanguageServerDatabase,
+    params: TextDocumentItem,
+) {
+    let diagnostics = {
+        // let workspace = &mut workspace.lock().await;
+        // let db = &mut db.lock().await;
+        let input = workspace
+            .input_from_file_path(
+                db,
+                params
+                    .uri
+                    .to_file_path()
+                    .expect("Failed to convert URI to file path")
+                    .to_str()
+                    .expect("Failed to convert file path to string"),
+            )
+            .unwrap();
+        let _ = input.sync(db, Some(params.text));
+        get_diagnostics(db, workspace, params.uri.clone())
+    };
+
+    // let client = client.lock().await;
+    let diagnostics = diagnostics
+        .unwrap()
+        .into_iter()
+        .map(|(uri, diags)| client.publish_diagnostics(uri, diags, None))
+        .collect::<Vec<_>>();
+
+    futures::future::join_all(diagnostics).await;
 }
--- crates/language-server/src/language_server.rs
+++ crates/language-server/src/language_server.rs
@@ -7,46 +7,6 @@ use lsp_types::{
 
 use tower_lsp::{jsonrpc::Result, Client, LanguageServer};
 
-use crate::{
-    capabilities::server_capabilities,
-};
-
-// This is replaced by the `dispatcher` procedural macro!
-// struct Dispatch {
-//     tx_initialize: tokio::sync::mpsc::Sender<InitializeParams>,
-//     initialize_stream: tokio_stream::wrappers::ReceiverStream<InitializeParams>,
-//     tx_did_open: tokio::sync::mpsc::Sender<TextDocumentItem>,
-//     did_open_stream: tokio_stream::wrappers::ReceiverStream<TextDocumentItem>,
-//     tx_did_change: tokio::sync::mpsc::Sender<TextDocumentItem>,
-//     did_change_stream: tokio_stream::wrappers::ReceiverStream<TextDocumentItem>,
-//     tx_did_close: tokio::sync::mpsc::Sender<DidCloseTextDocumentParams>,
-//     did_close_stream: tokio_stream::wrappers::ReceiverStream<DidCloseTextDocumentParams>,
-//     tx_did_change_watched_files: tokio::sync::mpsc::Sender<DidChangeWatchedFilesParams>,
-//     did_change_watched_files_stream: tokio_stream::wrappers::ReceiverStream<DidChangeWatchedFilesParams>,
-// }
-
-// impl Dispatch {
-//     fn new() -> Self {
-//         let (tx_initialize, rx_initialize) = tokio::sync::mpsc::channel(16);
-//         let (tx_did_open, rx_did_open) = tokio::sync::mpsc::channel(16);
-//         let (tx_did_change, rx_did_change) = tokio::sync::mpsc::channel(16);
-//         let (tx_did_close, rx_did_close) = tokio::sync::mpsc::channel(16);
-//         let (tx_did_change_watched_files, rx_did_change_watched_files) = tokio::sync::mpsc::channel(16);
-//         Self {
-//             tx_initialize,
-//             tx_did_open,
-//             tx_did_change,
-//             tx_did_close,
-//             tx_did_change_watched_files,
-//             initialize_stream: tokio_stream::wrappers::ReceiverStream::new(rx_initialize),
-//             did_open_stream: tokio_stream::wrappers::ReceiverStream::new(rx_did_open),
-//             did_close_stream: tokio_stream::wrappers::ReceiverStream::new(rx_did_close),
-//             did_change_stream: tokio_stream::wrappers::ReceiverStream::new(rx_did_change),
-//             did_change_watched_files_stream: tokio_stream::wrappers::ReceiverStream::new(rx_did_change_watched_files),
-//         }
-//     }
-// }
-
 pub(crate) struct Server {
     pub(crate) dispatch: LspChannels,
     pub(crate) client: Arc<tokio::sync::Mutex<Client>>,
@@ -99,147 +59,18 @@ impl LanguageServer for Server {
     }
 
     async fn did_open(&self, params: lsp_types::DidOpenTextDocumentParams) {
-        let _ = self.dispatch.did_open_tx.send(params);
-        // self.tx.send(Box::new(params));
-        // info!("did open: {:?}", params);
-        // {
-        //     let workspace = &mut self.workspace.lock().unwrap();
-        //     let db = &mut self.db.lock().unwrap();
-        //     let _ = workspace.sync(db);
-        // }
-
-        // on_change(
-        //     self,
-        //     TextDocumentItem {
-        //         uri: params.text_document.uri,
-        //         language_id: LANGUAGE_ID.to_string(),
-        //         version: params.text_document.version,
-        //         text: params.text_document.text,
-        //     },
-        // )
-        // .await;
+        self.dispatch.dispatch_did_open(params);
     }
 
     async fn did_change(&self, params: lsp_types::DidChangeTextDocumentParams) {
-        let _ = self.dispatch.did_change_tx.send(params);
-        // info!("did change: {:?}", params);
-        // on_change(
-        //     self,
-        //     TextDocumentItem {
-        //         uri: params.text_document.uri,
-        //         language_id: LANGUAGE_ID.to_string(),
-        //         version: params.text_document.version,
-        //         text: params.content_changes[0].text.clone(),
-        //     },
-        // )
-        // .await;
+        self.dispatch.dispatch_did_change(params);
     }
 
-    // Currently this is used to handle document renaming since the "document open" handler is called
-    // before the "document was renamed" handler.
-    //
-    // The fix: handle document renaming more explicitly in the "will rename" flow, along with the document
-    // rename refactor.
     async fn did_close(&self, params: DidCloseTextDocumentParams) {
-        let _ = self.dispatch.did_close_tx.send(params);
-        // let workspace = &mut self.workspace.lock().unwrap();
-        // // let workspace = &mut workspace.lock().await;
-        // let db = &mut self.db.lock().unwrap();
-        // // let db = &mut db.lock().await;
-
-        // let input = workspace
-        //     .input_from_file_path(
-        //         db,
-        //         params
-        //             .text_document
-        //             .uri
-        //             .to_file_path()
-        //             .unwrap()
-        //             .to_str()
-        //             .unwrap(),
-        //     )
-        //     .unwrap();
-        // let _ = input.sync(db, None);
+        self.dispatch.dispatch_did_close(params);
     }
-    async fn did_change_watched_files(&self, params: DidChangeWatchedFilesParams) {
-        let _ = self.dispatch.did_change_watched_files_tx.send(params);
-        // let changes = params.changes;
-        // for change in changes {
-        //     let uri = change.uri;
-        //     let path = uri.to_file_path().unwrap();
 
-        //     match change.typ {
-        //         lsp_types::FileChangeType::CREATED => {
-        //             // TODO: handle this more carefully!
-        //             // this is inefficient, a hack for now
-        //             let workspace = &mut self.workspace.lock().unwrap();
-        //             let db = &mut self.db.lock().unwrap();
-        //             let _ = workspace.sync(db);
-        //             let input = workspace
-        //                 .input_from_file_path(db, path.to_str().unwrap())
-        //                 .unwrap();
-        //             let _ = input.sync(db, None);
-        //         }
-        //         lsp_types::FileChangeType::CHANGED => {
-        //             let workspace = &mut self.workspace.lock().unwrap();
-        //             let db = &mut self.db.lock().unwrap();
-        //             let input = workspace
-        //                 .input_from_file_path(db, path.to_str().unwrap())
-        //                 .unwrap();
-        //             let _ = input.sync(db, None);
-        //         }
-        //         lsp_types::FileChangeType::DELETED => {
-        //             // TODO: handle this more carefully!
-        //             // this is inefficient, a hack for now
-        //             let workspace = &mut self.workspace.lock().unwrap();
-        //             let db = &mut self.db.lock().unwrap();
-        //             let _ = workspace.sync(db);
-        //         }
-        //         _ => {}
-        //     }
-        //     // collect diagnostics for the file
-        //     if change.typ != lsp_types::FileChangeType::DELETED {
-        //         let text = std::fs::read_to_string(path).unwrap();
-        //         on_change(
-        //             self,
-        //             TextDocumentItem {
-        //                 uri: uri.clone(),
-        //                 language_id: LANGUAGE_ID.to_string(),
-        //                 version: 0,
-        //                 text,
-        //             },
-        //         )
-        //         .await;
-        //     }
-        // }
+    async fn did_change_watched_files(&self, params: DidChangeWatchedFilesParams) {
+        self.dispatch.dispatch_did_change_watched_files(params);
     }
 }
-
-// async fn on_change(backend: &Backend, params: TextDocumentItem) {
-//     let diagnostics = {
-//         let workspace = &mut backend.workspace.lock().unwrap();
-//         let db = &mut backend.db.lock().unwrap();
-//         let input = workspace
-//             .input_from_file_path(
-//                 db,
-//                 params
-//                     .uri
-//                     .to_file_path()
-//                     .expect("Failed to convert URI to file path")
-//                     .to_str()
-//                     .expect("Failed to convert file path to string"),
-//             )
-//             .unwrap();
-//         let _ = input.sync(db, Some(params.text));
-//         get_diagnostics(db, workspace, params.uri.clone())
-//     };
-
-//     let client = backend.client.lock().await;
-//     let diagnostics = diagnostics
-//         .unwrap()
-//         .into_iter()
-//         .map(|(uri, diags)| client.publish_diagnostics(uri, diags, None))
-//         .collect::<Vec<_>>();
-
-//     futures::future::join_all(diagnostics).await;
-// }
