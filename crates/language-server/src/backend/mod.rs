7f0b3f94cb7109fa22aa3ddd3a33440a7fdb47ee
Micah <micahscopes@gmail.com>
2024-03-28 18:09:13 -0500

    no need for workspace lock

--- crates/language-server/src/backend/mod.rs
+++ crates/language-server/src/backend/mod.rs
@@ -1,8 +1,6 @@
 pub(crate) mod db;
 pub(crate) mod workspace;
 use db::LanguageServerDatabase;
-use std::sync::Arc;
-use tokio::sync::RwLock;
 use workspace::Workspace;
 
 use tower_lsp::Client;
@@ -10,14 +8,14 @@ use tower_lsp::Client;
 pub struct Backend {
     pub(super) client: Client,
     pub(super) db: LanguageServerDatabase,
-    pub(super) workspace: Arc<RwLock<Workspace>>,
+    pub(super) workspace: Workspace,
     pub(super) workers: tokio::runtime::Runtime,
 }
 
 impl Backend {
     pub fn new(client: Client) -> Self {
         let db = LanguageServerDatabase::default();
-        let workspace = Arc::new(RwLock::new(Workspace::default()));
+        let workspace = Workspace::default();
 
         let workers = tokio::runtime::Builder::new_multi_thread()
             .worker_threads(4)
