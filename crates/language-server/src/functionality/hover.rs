17272e27e62d8ce302a5b0ae4264e0ebd2453d53
Micah <micahscopes@gmail.com>
2024-04-01 02:02:32 -0500

    Applied some suggestions from review

--- crates/language-server/src/functionality/hover.rs
+++ crates/language-server/src/functionality/hover.rs
@@ -1,22 +1,19 @@
-use common::{InputDb, InputFile};
+use common::InputFile;
 use hir::lower::map_file_to_mod;
-use hir::LowerHirDb;
-use hir::{HirDb, SpannedHirDb};
 
 use lsp_types::Hover;
 use tracing::info;
 
-use salsa::Snapshot;
-
 use tower_lsp::jsonrpc::Result;
 
-use crate::{backend::db::LanguageServerDatabase, util::to_offset_from_position};
+use crate::backend::db::LanguageServerDb;
+use crate::util::to_offset_from_position;
 
 use super::goto::{get_goto_target_scopes_for_cursor, Cursor};
 use super::item_info::{get_item_definition_markdown, get_item_docstring, get_item_path_markdown};
 
 pub fn hover_helper(
-    db: Snapshot<LanguageServerDatabase>,
+    db: &dyn LanguageServerDb,
     input: InputFile,
     params: lsp_types::HoverParams,
 ) -> Result<Option<Hover>> {
@@ -29,17 +26,16 @@ pub fn hover_helper(
     );
 
     let top_mod = map_file_to_mod(db.as_lower_hir_db(), input);
-    let goto_info = &get_goto_target_scopes_for_cursor(db.as_language_server_db(), top_mod, cursor)
-        .unwrap_or_default();
+    let goto_info = &get_goto_target_scopes_for_cursor(db, top_mod, cursor).unwrap_or_default();
 
     let hir_db = db.as_hir_db();
     let scopes_info = goto_info
         .iter()
         .map(|scope| {
             let item = &scope.item();
-            let pretty_path = get_item_path_markdown(item, hir_db);
-            let definition_source = get_item_definition_markdown(item, db.as_spanned_hir_db());
-            let docs = get_item_docstring(item, hir_db);
+            let pretty_path = get_item_path_markdown(*item, hir_db);
+            let definition_source = get_item_definition_markdown(*item, db.as_spanned_hir_db());
+            let docs = get_item_docstring(*item, hir_db);
 
             let result = [pretty_path, definition_source, docs]
                 .iter()
--- crates/language-server/src/functionality/item_info.rs
+++ crates/language-server/src/functionality/item_info.rs
@@ -4,20 +4,20 @@ use hir::{
     HirDb, SpannedHirDb,
 };
 
-pub fn get_item_docstring(item: &ItemKind, hir_db: &dyn HirDb) -> Option<String> {
+pub fn get_item_docstring(item: ItemKind, hir_db: &dyn HirDb) -> Option<String> {
     let docstring = match item {
-        ItemKind::Func(func) => func.attributes(hir_db).data(hir_db).clone(),
-        ItemKind::Mod(mod_) => mod_.attributes(hir_db).data(hir_db).clone(),
-        ItemKind::Struct(struct_) => struct_.attributes(hir_db).data(hir_db).clone(),
-        ItemKind::Enum(enum_) => enum_.attributes(hir_db).data(hir_db).clone(),
-        ItemKind::TypeAlias(type_alias) => type_alias.attributes(hir_db).data(hir_db).clone(),
-        ItemKind::Trait(trait_) => trait_.attributes(hir_db).data(hir_db).clone(),
-        ItemKind::Impl(impl_) => impl_.attributes(hir_db).data(hir_db).clone(),
+        ItemKind::Func(func) => func.attributes(hir_db).data(hir_db),
+        ItemKind::Mod(mod_) => mod_.attributes(hir_db).data(hir_db),
+        ItemKind::Struct(struct_) => struct_.attributes(hir_db).data(hir_db),
+        ItemKind::Enum(enum_) => enum_.attributes(hir_db).data(hir_db),
+        ItemKind::TypeAlias(type_alias) => type_alias.attributes(hir_db).data(hir_db),
+        ItemKind::Trait(trait_) => trait_.attributes(hir_db).data(hir_db),
+        ItemKind::Impl(impl_) => impl_.attributes(hir_db).data(hir_db),
         // ItemKind::Body(body) => body.attributes(hir_db).data(hir_db).clone(),
         // ItemKind::Const(const_) => const_.attributes(hir_db).data(hir_db).clone(),
         // ItemKind::Use(use_) => use_.attributes(hir_db).data(hir_db).clone(),
-        ItemKind::Contract(contract) => contract.attributes(hir_db).data(hir_db).clone(),
-        _ => vec![],
+        ItemKind::Contract(contract) => contract.attributes(hir_db).data(hir_db),
+        _ => return None,
     }
     .iter()
     .filter_map(|attr| {
@@ -36,13 +36,13 @@ pub fn get_item_docstring(item: &ItemKind, hir_db: &dyn HirDb) -> Option<String>
     }
 }
 
-pub fn get_item_path_markdown(item: &ItemKind, hir_db: &dyn HirDb) -> Option<String> {
+pub fn get_item_path_markdown(item: ItemKind, hir_db: &dyn HirDb) -> Option<String> {
     item.scope()
         .pretty_path(hir_db)
         .map(|path| format!("```fe\n{}\n```", path))
 }
 
-pub fn get_item_definition_markdown(item: &ItemKind, db: &dyn SpannedHirDb) -> Option<String> {
+pub fn get_item_definition_markdown(item: ItemKind, db: &dyn SpannedHirDb) -> Option<String> {
     // TODO: use pending AST features to get the definition without all this text manipulation
     let hir_db = db.as_hir_db();
     let span = item.lazy_span().resolve(db)?;
