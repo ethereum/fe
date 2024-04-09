57fa1ad65d6311e93071bf989190dd0ab074e4a5
Yoshitomo Nakanishi <yurayura.rounin.3@gmail.com>
2024-04-09 23:46:49 +0200

    Cleanup trait impl method comparison

---
+++ crates/hir-analysis/src/ty/method_cmp.rs
@@ -0,0 +1,281 @@
+use super::{
+    binder::Binder,
+    constraint::collect_func_def_constraints,
+    constraint_solver::{is_goal_satisfiable, GoalSatisfiability},
+    diagnostics::{ImplDiag, TyDiagCollection},
+    trait_def::{TraitInstId, TraitMethod},
+    ty_def::{FuncDef, TyId},
+};
+use crate::HirAnalysisDb;
+
+pub(super) fn compare_impl_method(
+    db: &dyn HirAnalysisDb,
+    impl_m: Binder<FuncDef>,
+    trait_m: Binder<TraitMethod>,
+    trait_inst: TraitInstId,
+    sink: &mut Vec<TyDiagCollection>,
+) {
+    if !compare_generic_param_num(db, impl_m.skip_binder(), trait_m.skip_binder().0, sink) {
+        return;
+    }
+
+    if !compare_generic_param_kind(db, impl_m.skip_binder(), trait_m.skip_binder().0, sink) {
+        return;
+    }
+
+    // Instantiate trait method with the type arguments of the trait instance and
+    // the impl method type parameters.
+    // This should be done after ensuring that the number of generic parameters and
+    // their kinds are the same.
+    let impl_m = impl_m.instantiate_identity();
+    let subst: Vec<_> = trait_inst
+        .args(db)
+        .iter()
+        .chain(impl_m.original_params(db).iter())
+        .copied()
+        .collect();
+    let trait_m = trait_m.instantiate(db, &subst).0;
+
+    if !compare_arity(db, impl_m, trait_m, sink) {
+        return;
+    }
+
+    // Compare the argument labels, argument types, and return type of the impl
+    // method with the trait method.
+    let mut err = !compare_arg_label(db, impl_m, trait_m, sink);
+    err |= !compare_ty(db, impl_m, trait_m, sink);
+    if err {
+        return;
+    }
+
+    compare_constraints(db, impl_m, trait_m, &subst, sink);
+}
+
+/// Checks if the number of generic parameters of the implemented method is the
+/// same as the number of generic parameters of the trait method.
+/// Returns `false` if the comparison fails.
+fn compare_generic_param_num(
+    db: &dyn HirAnalysisDb,
+    impl_m: FuncDef,
+    trait_m: FuncDef,
+    sink: &mut Vec<TyDiagCollection>,
+) -> bool {
+    let impl_params = impl_m.original_params(db);
+    let trait_params = trait_m.original_params(db);
+
+    let hir_impl_m = impl_m.hir_func(db);
+    let hir_trait_m = trait_m.hir_func(db);
+
+    if impl_params.len() == trait_params.len() {
+        true
+    } else {
+        sink.push(
+            ImplDiag::method_param_num_mismatch(
+                hir_impl_m.lazy_span().name().into(),
+                trait_params.len(),
+                impl_params.len(),
+            )
+            .into(),
+        );
+        false
+    }
+}
+
+/// Checks if the generic parameter kinds are the same.
+/// Returns `false` if the comparison fails.
+fn compare_generic_param_kind(
+    db: &dyn HirAnalysisDb,
+    impl_m: FuncDef,
+    trait_m: FuncDef,
+    sink: &mut Vec<TyDiagCollection>,
+) -> bool {
+    let mut err = false;
+    for (idx, (&trait_m_param, &impl_m_param)) in trait_m
+        .original_params(db)
+        .iter()
+        .zip(impl_m.original_params(db))
+        .enumerate()
+    {
+        let trait_m_kind = trait_m_param.kind(db);
+        let impl_m_kind = impl_m_param.kind(db);
+
+        if !trait_m_kind.does_match(impl_m_kind) {
+            let span = impl_m
+                .hir_func(db)
+                .lazy_span()
+                .generic_params()
+                .param(idx)
+                .into();
+
+            sink.push(ImplDiag::method_param_kind_mismatch(span, trait_m_kind, impl_m_kind).into());
+            err = true;
+        }
+    }
+
+    !err
+}
+
+/// Checks if the arity of the implemented method is the same as the arity of
+/// the trait method.
+/// Returns `false` if the comparison fails.
+fn compare_arity(
+    db: &dyn HirAnalysisDb,
+    impl_m: FuncDef,
+    trait_m: FuncDef,
+    sink: &mut Vec<TyDiagCollection>,
+) -> bool {
+    let impl_m_arity = impl_m.arg_tys(db).len();
+    let trait_m_arity = trait_m.arg_tys(db).len();
+
+    let hir_impl_method = impl_m.hir_func(db);
+    let hir_trait_method = trait_m.hir_func(db);
+
+    // Checks if the arity are the same.
+    if impl_m_arity == trait_m_arity {
+        true
+    } else {
+        sink.push(
+            ImplDiag::method_arg_num_mismatch(
+                hir_impl_method.lazy_span().params_moved().into(),
+                trait_m_arity,
+                impl_m_arity,
+            )
+            .into(),
+        );
+        false
+    }
+}
+
+/// Checks if the argument labels of the implemented method are the same as the
+/// argument labels of the trait method.
+/// Returns `false` if the comparison fails.
+fn compare_arg_label(
+    db: &dyn HirAnalysisDb,
+    impl_m: FuncDef,
+    trait_m: FuncDef,
+    sink: &mut Vec<TyDiagCollection>,
+) -> bool {
+    let mut err = false;
+    let hir_impl_method = impl_m.hir_func(db);
+    let hir_trait_method = trait_m.hir_func(db);
+
+    for (idx, (expected_param, method_param)) in trait_m
+        .hir_params(db)
+        .iter()
+        .zip(impl_m.hir_params(db))
+        .enumerate()
+    {
+        let Some(expected_label) = expected_param
+            .label
+            .or_else(|| expected_param.name.to_opt())
+        else {
+            continue;
+        };
+
+        let Some(method_label) = method_param.label.or_else(|| method_param.name.to_opt()) else {
+            continue;
+        };
+
+        if expected_label != method_label {
+            let primary = hir_impl_method.lazy_span().params_moved().param(idx).into();
+            let sub = hir_trait_method
+                .lazy_span()
+                .params_moved()
+                .param(idx)
+                .into();
+
+            sink.push(
+                ImplDiag::method_arg_label_mismatch(db, primary, sub, expected_label, method_label)
+                    .into(),
+            );
+            err = true;
+        }
+    }
+
+    !err
+}
+
+/// Checks if the argument types and return type of the implemented method are
+/// the same as the argument types and return type of the trait method.
+/// Returns `false` if the comparison fails.
+fn compare_ty(
+    db: &dyn HirAnalysisDb,
+    impl_m: FuncDef,
+    trait_m: FuncDef,
+    sink: &mut Vec<TyDiagCollection>,
+) -> bool {
+    let mut err = false;
+    let impl_m_arg_tys = impl_m.arg_tys(db);
+    let trait_m_arg_tys = trait_m.arg_tys(db);
+
+    for (idx, (&trait_m_ty, &impl_m_ty)) in trait_m_arg_tys.iter().zip(impl_m_arg_tys).enumerate() {
+        if !impl_m_ty.contains_invalid(db) && trait_m_ty != impl_m_ty {
+            let span = impl_m
+                .hir_func(db)
+                .lazy_span()
+                .params_moved()
+                .param(idx)
+                .into();
+            sink.push(ImplDiag::method_arg_ty_mismatch(db, span, trait_m_ty, impl_m_ty).into());
+            err = true;
+        }
+    }
+
+    let impl_m_ret_ty = impl_m.ret_ty(db);
+    let trait_m_ret_ty = trait_m.ret_ty(db);
+    if !impl_m_ret_ty.contains_invalid(db) && trait_m_ret_ty != impl_m_ret_ty {
+        sink.push(
+            ImplDiag::method_ret_type_mismatch(
+                db,
+                impl_m.hir_func(db).lazy_span().ret_ty().into(),
+                trait_m_ret_ty,
+                impl_m_ret_ty,
+            )
+            .into(),
+        );
+
+        err = true;
+    }
+
+    !err
+}
+
+/// Checks if the method constraints are stricter than the trait constraints.
+/// This check is performed by checking if the `impl_method` constraints are
+/// satisfied under the assumptions that is obtained from the `expected_method`
+/// constraints.
+/// Returns `false` if the comparison fails.
+fn compare_constraints(
+    db: &dyn HirAnalysisDb,
+    impl_m: FuncDef,
+    trait_m: FuncDef,
+    subst: &[TyId],
+    sink: &mut Vec<TyDiagCollection>,
+) -> bool {
+    let impl_m_constraints = collect_func_def_constraints(db, impl_m).instantiate_identity();
+    let trait_m_constraints = collect_func_def_constraints(db, trait_m).instantiate(db, subst);
+    let mut unsatisfied_goals = vec![];
+    for &goal in impl_m_constraints.predicates(db) {
+        if !matches!(
+            is_goal_satisfiable(db, goal, trait_m_constraints),
+            GoalSatisfiability::Satisfied
+        ) {
+            unsatisfied_goals.push(goal);
+        }
+    }
+
+    if unsatisfied_goals.is_empty() {
+        true
+    } else {
+        unsatisfied_goals.sort_by_key(|goal| goal.ty(db).pretty_print(db));
+        sink.push(
+            ImplDiag::method_stricter_bound(
+                db,
+                impl_m.hir_func(db).lazy_span().name().into(),
+                &unsatisfied_goals,
+            )
+            .into(),
+        );
+        false
+    }
+}
