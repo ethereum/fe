89a6b619606be86f620fa5a6c441f238cab15e0d
Grant  Wuerker <grantwuerker@pop-os.localdomain>
2025-02-17 13:38:45 -0700

    hacking

--- crates/resolver/src/ingot/graph.rs
+++ crates/resolver/src/ingot/graph.rs
@@ -1,13 +1,11 @@
 use core::panic;
-use std::env::current_dir;
 use std::mem::take;
-use std::{fs, io};
 
 use camino::{Utf8Path, Utf8PathBuf};
 use common::indexmap::{IndexMap, IndexSet};
 use glob::glob;
+use petgraph::dot;
 use petgraph::visit::EdgeRef;
-use petgraph::{dot, graph};
 use petgraph::{
     dot::Dot,
     graph::{DiGraph, NodeIndex},
@@ -107,11 +105,12 @@ pub enum Error {
 
 #[derive(Debug)]
 pub enum Diagnostic {
-    DepenendencyTargetPathDoesNotExist {
+    TargetPathDoesNotExist {
         source_path: Utf8PathBuf,
         target_path: Utf8PathBuf,
     },
-    ConfigResolutionError(Utf8PathBuf, config::Error),
+    TargetConfigResolutionError(Utf8PathBuf, config::Error),
+    LocalConfigResolutionError(Utf8PathBuf, config::Error),
 }
 
 impl Resolver for GraphResolver {
@@ -121,98 +120,80 @@ impl Resolver for GraphResolver {
     type Diagnostic = Diagnostic;
 
     fn resolve(&mut self, path: &Utf8PathBuf) -> Result<Graph, Error> {
-        if !path.exists() {
-            Err(Error::PathDoesNotExist)
-        } else {
-            // let local_path = path.canonicalize_utf8().unwrap();
+        let path = path.canonicalize_utf8().unwrap();
 
-            let mut graph = Graph::default();
+        let mut graph = Graph::default();
 
-            // let local_config = self
-            //     .config_resolver
-            // .resolve(&path)
-            // .map_err(Error::ConfigResolutionError)?;
+        let glob_path = path.join("**/fe.toml");
 
-            let glob_path = path.join("**/fe.toml");
-            // panic!("{}", glob_path.as_str());
-            // panic!("{:?}", current_dir().unwrap());
-
-            let local_configs: IndexMap<Utf8PathBuf, Config> = glob(glob_path.as_str())
-                .expect("failed to read glob pattern")
-                .filter_map(|entry| match entry {
-                    Ok(path) => match Utf8PathBuf::from_path_buf(path.to_path_buf()) {
-                        Ok(path) => match self.config_resolver.resolve(&path) {
-                            Ok(config) => Some((path.parent().unwrap().to_path_buf(), config)),
-                            Err(error) => {
-                                panic!("{}", error);
-                                self.diagnostics
-                                    .push(Diagnostic::ConfigResolutionError(path, error));
-                                None
-                            }
-                        },
-                        Err(path) => {
-                            // self.diagnostics.push(Diagnostic::NonUtf8Path(path));
-                            todo!();
-                            None
-                        }
-                    },
+        let local_configs: IndexMap<_, _> = glob(glob_path.as_str())
+            .expect("failed to read glob pattern")
+            .filter_map(|entry| {
+                let path = Utf8PathBuf::from_path_buf(entry.unwrap().to_path_buf())
+                    .unwrap()
+                    .canonicalize_utf8()
+                    .unwrap();
+                match self.config_resolver.resolve(&path) {
+                    Ok(config) => Some((path.parent().unwrap().to_path_buf(), config)),
                     Err(error) => {
-                        // self.diagnostics.push(Diagnostic::GlobError(error));
-                        panic!("{error}");
-                        todo!();
+                        self.diagnostics
+                            .push(Diagnostic::LocalConfigResolutionError(path, error));
                         None
                     }
-                })
-                .collect();
-            // panic!("{:?}", local_configs);
-            //
-            let mut unresolved_dependencies = vec![];
-
-            for (ingot_path, config) in local_configs.iter() {
-                graph.local_paths.insert(ingot_path.clone());
-                if let Some(dependencies) = &config.dependencies {
-                    unresolved_dependencies.push((ingot_path.clone(), dependencies.clone()));
                 }
+            })
+            .collect();
+
+        let mut unresolved_dependencies = vec![];
+
+        for (ingot_path, config) in local_configs.iter() {
+            graph.local_paths.insert(ingot_path.clone());
+            if let Some(dependencies) = &config.dependencies {
+                unresolved_dependencies.push((ingot_path.clone(), dependencies.clone()));
             }
-
-            graph.configs = local_configs;
-
-            while let Some((source_path, targets)) = unresolved_dependencies.pop() {
-                for (target_name, target_description) in targets {
-                    // add check for missing path
-                    let target_path = match target_description.files {
-                        FilesDescription::Path(path) => {
-                            source_path.join(path).canonicalize_utf8().unwrap()
-                        }
-                    };
-
-                    if !target_path.exists() {
-                        self.diagnostics
-                            .push(Diagnostic::DepenendencyTargetPathDoesNotExist {
-                                source_path: source_path.clone(),
-                                target_path: target_path.clone(),
-                            });
-                        continue;
-                    }
-
-                    if !graph.contains(&target_path) {
-                        match self.config_resolver.resolve(&target_path) {
-                            Ok(config) => {
-                                if let Some(dependencies) = config.dependencies {
-                                    unresolved_dependencies
-                                        .push((target_path.clone(), dependencies))
-                                }
-                            }
-                            Err(err) => panic!("{} {}", err, target_path),
-                        }
-                    }
-
-                    graph.add_dependency(target_name, &source_path, &target_path);
-                }
-            }
-
-            Ok(graph)
         }
+
+        graph.configs = local_configs;
+
+        while let Some((source_path, targets)) = unresolved_dependencies.pop() {
+            for (target_name, target_description) in targets {
+                let target_path = match target_description.files {
+                    FilesDescription::Path(path) => {
+                        source_path.join(path).canonicalize_utf8().unwrap()
+                    }
+                };
+
+                if !target_path.exists() {
+                    self.diagnostics.push(Diagnostic::TargetPathDoesNotExist {
+                        source_path: source_path.clone(),
+                        target_path: target_path.clone(),
+                    });
+                    continue;
+                }
+
+                if !graph.contains(&target_path) {
+                    match self.config_resolver.resolve(&target_path) {
+                        Ok(config) => {
+                            if let Some(dependencies) = config.dependencies.clone() {
+                                unresolved_dependencies.push((target_path.clone(), dependencies))
+                            }
+                            graph.configs.insert(source_path.clone(), config);
+                        }
+                        Err(error) => {
+                            self.diagnostics
+                                .push(Diagnostic::TargetConfigResolutionError(
+                                    target_path.clone(),
+                                    error,
+                                ))
+                        }
+                    }
+                }
+
+                graph.add_dependency(target_name, &source_path, &target_path);
+            }
+        }
+
+        Ok(graph)
     }
 
     fn take_diagnostics(&mut self) -> Vec<Self::Diagnostic> {
