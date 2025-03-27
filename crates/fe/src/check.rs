561a00e2799f166f39bd7aef531edfc9a448ee0a
Grant  Wuerker <grantwuerker@pop-os.localdomain>
2025-03-27 14:55:13 -0600

    Resolution handler API and ingot graph resolution.

---
+++ crates/fe/src/check.rs
@@ -0,0 +1,28 @@
+use camino::Utf8PathBuf;
+use common::{urlext::canonical_url, InputDb};
+use driver::DriverDataBase;
+
+pub fn check(path: &Utf8PathBuf) {
+    let mut db = DriverDataBase::default();
+    let ingot_url = canonical_url(path);
+    driver::init_workspace_ingot(&mut db, &ingot_url);
+
+    let ingot = db.workspace().containing_ingot(&db, &ingot_url).unwrap();
+    let diags = db.run_on_ingot(ingot);
+    if !diags.is_empty() {
+        eprintln!("errors in {ingot_url}");
+        diags.emit(&db);
+    }
+
+    for dependency_url in db.graph().dependency_urls(&db, &ingot_url) {
+        let ingot = db
+            .workspace()
+            .containing_ingot(&db, &dependency_url)
+            .unwrap();
+        let diags = db.run_on_ingot(ingot);
+        if !diags.is_empty() {
+            eprintln!("errors in {dependency_url}");
+            diags.emit(&db);
+        }
+    }
+}
--- crates/resolver/src/lib.rs
+++ crates/resolver/src/lib.rs
@@ -1,11 +1,42 @@
+pub mod files;
+pub mod graph;
 pub mod ingot;
 
-pub trait Resolver {
+pub trait Resolver: Sized {
     type Description;
     type Resource;
     type Error;
     type Diagnostic;
 
-    fn resolve(&mut self, description: &Self::Description) -> Result<Self::Resource, Self::Error>;
+    fn resolve<H>(
+        &mut self,
+        handler: &mut H,
+        description: &Self::Description,
+    ) -> Result<H::Item, Self::Error>
+    where
+        H: ResolutionHandler<Self>,
+    {
+        self.transient_resolve(description)
+            .map(|resource| handler.handle_resolution(description, resource))
+    }
+
+    fn transient_resolve(
+        &mut self,
+        description: &Self::Description,
+    ) -> Result<Self::Resource, Self::Error>;
+
     fn take_diagnostics(&mut self) -> Vec<Self::Diagnostic>;
+}
+
+pub trait ResolutionHandler<R>
+where
+    R: Resolver,
+{
+    type Item;
+
+    fn handle_resolution(
+        &mut self,
+        description: &R::Description,
+        resource: R::Resource,
+    ) -> Self::Item;
 }
