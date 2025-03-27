c9e8419b99d95886545ac6af38b8dcaccdb2cd20
Grant  Wuerker <grantwuerker@pop-os.localdomain>
2025-03-27 14:55:13 -0600

    Resolution handler API and ingot graph resolution.

--- crates/resolver/src/ingot.rs
+++ crates/resolver/src/ingot.rs
@@ -1,15 +1,24 @@
-use std::{fmt, fs};
+use std::collections::HashMap;
 
+<<<<<<< HEAD
 use camino::Utf8PathBuf;
 use config::{Config, ConfigResolver};
 use source_files::{SourceFiles, SourceFilesResolver};
+=======
+use common::config::{Config, IngotArguments};
+use smol_str::SmolStr;
+>>>>>>> 638b370b5 (Resolution handler API and ingot graph resolution.)
 use url::Url;
 
-use crate::Resolver;
+use crate::{
+    files::{File, FilesResolver},
+    graph::GraphResolverImpl,
+    ResolutionHandler,
+};
 
-pub mod config;
-pub mod source_files;
+pub type IngotGraphResolver<NH> = GraphResolverImpl<FilesResolver, NH, (SmolStr, IngotArguments)>;
 
+<<<<<<< HEAD
 #[derive(Debug)]
 pub enum Ingot {
     SingleFile {
@@ -35,12 +44,34 @@ pub enum Error {
     IngotIsEmpty(Url),
     StandaloneFileReadError { url: Url, error: std::io::Error },
 }
-
-#[derive(Default)]
-pub struct IngotResolver {
-    diagnostics: Vec<Diagnostic>,
+=======
+pub fn basic_ingot_graph_resolver() -> IngotGraphResolver<BasicIngotNodeHandler> {
+    GraphResolverImpl::new(
+        FilesResolver::exact_file("fe.toml".into()),
+        BasicIngotNodeHandler::default(),
+    )
 }
 
+pub fn ingot_graph_resolver<NH>(node_handler: NH) -> IngotGraphResolver<NH> {
+    let files_resolver = FilesResolver::with_patterns(&["fe.toml", "src/**/*.fe"]);
+    GraphResolverImpl::new(files_resolver, node_handler)
+}
+
+// #[derive(Debug)]
+// pub struct IngotConfigDoesNotExist;
+//
+// #[derive(Debug)]
+// pub struct UnresolvedDependency;
+
+pub type BasicIngotGraphResolver = IngotGraphResolver<BasicIngotNodeHandler>;
+>>>>>>> 638b370b5 (Resolution handler API and ingot graph resolution.)
+
+#[derive(Default)]
+pub struct BasicIngotNodeHandler {
+    pub configs: HashMap<Url, Config>,
+}
+
+<<<<<<< HEAD
 impl Resolver for IngotResolver {
     type Description = Url;
     type Resource = Ingot;
@@ -134,6 +165,29 @@ impl fmt::Display for Diagnostic {
                 }
                 Ok(())
             }
+=======
+impl ResolutionHandler<FilesResolver> for BasicIngotNodeHandler {
+    type Item = Vec<(Url, (SmolStr, IngotArguments))>;
+
+    fn handle_resolution(&mut self, ingot_url: &Url, mut files: Vec<File>) -> Self::Item {
+        if let Some(file) = files.pop() {
+            let config = Config::from_string(file.content);
+            self.configs.insert(ingot_url.clone(), config.clone());
+            config
+                .based_dependencies(ingot_url)
+                .into_iter()
+                .map(|based_dependency| {
+                    (
+                        // Node weight
+                        based_dependency.url,
+                        // Node edge
+                        (based_dependency.alias, based_dependency.arguments),
+                    )
+                })
+                .collect()
+        } else {
+            vec![]
+>>>>>>> 638b370b5 (Resolution handler API and ingot graph resolution.)
         }
     }
 }
