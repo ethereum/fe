9144ce2286ebefdd100a28593f35c0439b4a0a5d
Sean Billig <sean.billig@gmail.com>
2025-04-29 15:11:49 -0700

    remove defunct impl_db_traits macro

--- crates/common/src/lib.rs
+++ crates/common/src/lib.rs
@@ -5,7 +5,7 @@ pub mod ingot;
 pub mod input;
 pub use input::{InputFile, InputIngot};
 
-#[derive(Embed)]
+#[derive(rust_embed::Embed)]
 #[folder = "../../library/core"]
 pub(crate) struct Core;
 
@@ -14,17 +14,3 @@ pub trait InputDb: salsa::Database {}
 
 #[salsa::db]
 impl<T> InputDb for T where T: salsa::Database {}
-
-#[doc(hidden)]
-pub use paste::paste;
-use rust_embed::Embed;
-
-#[macro_export]
-macro_rules! impl_db_traits {
-    ($db_type:ty, $($trait_name:ident),+ $(,)?) => {
-        #[salsa::db]
-        impl salsa::Database for $db_type {
-            fn salsa_event(&self, _event: &dyn Fn() -> salsa::Event) {}
-        }
-    };
-}
